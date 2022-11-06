41b0d49fba19cd7a451b99d36af687f2381942b2
Juan Martinez <martinezalonsojn@gmail.com>
2022-11-06 22:58:11 +0100

    DEV-29 fully functional frontend for MVP-2

--- MVP-2/static/src/receiveTableBuilder.ts
+++ MVP-2/static/src/receiveTableBuilder.ts
@@ -1,31 +1,41 @@
 import { Measurement, Packet } from "./modals";
 
-var packets = new Map<number, Packet>();
+var globalPackets = new Map<number, Packet>();
 
-export function updateReceiveTable(packet: Packet) {
-  packets.set(packet.id, packet);
+export function updateReceiveTable(packets: Map<number, Packet>) {
+  updatePackets(packets);
   let tableBody = document.getElementById(
     "tableBody"
   ) as HTMLTableSectionElement;
   tableBody.textContent = "";
+  for (let [_, packet] of globalPackets) {
+    addPacketToTable(tableBody, packet);
+  }
+}
+
+function updatePackets(packets: Map<number, Packet>) {
   for (let [id, packet] of packets) {
-    let packetRow = createPacketRow(packet);
-    tableBody.append(packetRow);
-    let measurementsRows = createMeasurementRows(packet.measurements);
-    for (let row of measurementsRows) {
-      tableBody.append(row);
-    }
+    globalPackets.set(id, packet);
+  }
+}
+
+function addPacketToTable(tableBody: HTMLTableSectionElement, packet: Packet) {
+  let packetRow = createPacketRow(packet);
+  tableBody.append(packetRow);
+  let measurementsRows = createMeasurementRows(packet.measurements);
+  for (let row of measurementsRows) {
+    tableBody.append(row);
   }
 }
 
 function createPacketRow(packet: Packet): HTMLTableRowElement {
   let row = document.createElement("tr");
   let id_td = document.createElement("td");
-  id_td.innerHTML = packet.id.toString();
+  id_td.innerHTML = packet.id.toString(10);
   let name_td = document.createElement("td");
   name_td.innerHTML = packet.name.toString();
   let hexValue_td = document.createElement("td");
-  hexValue_td.innerHTML = packet.hexValue.toString(16);
+  hexValue_td.innerHTML = packet.hexValue.toString();
   let count_td = document.createElement("td");
   count_td.innerHTML = packet.count.toString();
   let cycleTime_td = document.createElement("td");
