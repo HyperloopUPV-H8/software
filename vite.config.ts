c9e821a4e57363708dcc1e98ab7debf17732db35
Sergio <smorsua@teleco.upv.es>
2022-11-11 19:11:41 +0100

    Configured absolute imports

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ export default defineConfig({
   resolve: {
     alias: {
       "@components": path.resolve(__dirname, "./src/components"),
+      "@models": path.resolve(__dirname, "./src/models"),
+      "@adapters": path.resolve(__dirname, "./src/adapters"),
+      "@context": path.resolve(__dirname, "./src/context"),
     },
   },
 });
