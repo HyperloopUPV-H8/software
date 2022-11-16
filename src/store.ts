07ac5450ea197653f43a26d4fe72a629d701adf3
Sergio <smorsua@teleco.upv.es>
2022-11-16 18:35:12 +0100

    Added slice to store

--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import podDataReducer from "@slices/podDataSlice";
 import orderReducer from "@slices/ordersSlice";
+import connectionsReducer from "@slices/connectionsSlice";
 
 export const store = configureStore({
   reducer: {
     podData: podDataReducer,
     orders: orderReducer,
+    connections: connectionsReducer,
   },
 });
 
