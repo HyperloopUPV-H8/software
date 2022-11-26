cb3807691d9ebd57b004892048d5114135b53992
lipezaballa <lipezaballa@gmail.com>
2022-11-26 20:06:39 +0100

    DEV-44 Changing dropdown icon, using global styles

--- src/components/MessageLogger/lineMessage.tsx
+++ src/components/MessageLogger/lineMessage.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Message } from "@components/MessageLogger/structs/Message";
 import styles from "@components/MessageLogger/LineMessage.module.scss"
+import {IoIosArrowDropdownCircle} from 'react-icons/io'
 
 
 interface Props {
@@ -21,7 +22,7 @@ export const LineMessage = ({ message, count }: Props) => {
         <li className={styles.lineMsg} key={message.id}  style={{
                 whiteSpace: isSelected ? 'normal' : 'nowrap'
             }}>
-            <img id={styles.dropDownArrow} src="https://cdnjs.cloudflare.com/ajax/libs/ionicons/4.6.3/collection/icon/svg/ios-arrow-dropdown-circle.svg" onClick={dropDownLine} style={{
+            <IoIosArrowDropdownCircle id={styles.dropDownArrowIcon} onClick={dropDownLine} style={{
                 transform: isSelected ? 'none' : 'rotate(270deg)'
             }}/>
             <label id={styles.idMsg}>{message.id}: </label>
