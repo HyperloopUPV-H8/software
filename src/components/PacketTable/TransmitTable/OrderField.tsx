33279afdae8426ba6c7074e1455b416737bb2632
Sergio <smorsua@teleco.upv.es>
2022-11-27 17:07:32 +0100

    Removed comments

--- src/components/PacketTable/TransmitTable/OrderField.tsx
+++ src/components/PacketTable/TransmitTable/OrderField.tsx
@@ -9,23 +9,6 @@ type Props = {
 };
 
 export const OrderField = ({ name, valueType, onChange }: Props) => {
-  //FIXME: hacer con desplazamiento
-  const isValueValid = (value: string | number): boolean => {
-    //   switch (valueType) {
-    // case "uint8": return (Number.isInteger(value) && value >= 0 && value <= 255)
-    // case "uint16": return (Number.isInteger(value) && value >= 0 && value <= 65535)
-    // case "uint32": return (Number.isInteger(value) && value >= 0 && value <= 4294967295)
-    // case "uint64": return (Number.isInteger(value) && value >= 0 && value <= 1.8446744e+19)
-    // case "int8": return (Number.isInteger(value) && value >= -128 && value <= 127)
-    // case "int16": return (Number.isInteger(value) && value >= -32768 && value <= 32767)
-    // case "int32": return (Number.isInteger(value) && value >= -9.223372e+18 && value <= 9.223372e+18)
-    // case "int64":
-    // case "float32":
-    // case "float64":
-    // case "bool":
-    return true;
-  };
-
   return (
     <tr id={styles.wrapper}>
       <td id={styles.name}>{name}</td>
