9f0fad8b6abc2791dac8afa8e7accca0b28fa0a1
lipezaballa <lipezaballa@gmail.com>
2022-11-30 21:52:31 +0100

    DEV-59 Last changes

--- src/components/ConnectionsTable/ConnectionsTable.tsx
+++ src/components/ConnectionsTable/ConnectionsTable.tsx
@@ -6,30 +6,28 @@ import { RootState } from "store";
 import { useDispatch, useSelector } from "react-redux";
 import { initializeMockConnections } from "@slices/connectionsSlice";
 
-
-
 export const ConnectionsTable = () => {
+  //Mock with state
+  //const [Connections, setConnections] = useState<Connection[]>(ConnectionsMock);
 
-    //Mock with state
-    //const [Connections, setConnections] = useState<Connection[]>(ConnectionsMock);
+  const connections = useSelector((state: RootState) => state.connections);
 
-    const connections = useSelector((state: RootState) => state.connections);
-    mockConnections()
+  mockConnections();
 
-    return (
-        <>
-            <ConnectionsList connectionsList={connections}/>
-        </>
-    )
-
-}
+  return (
+    <>
+      <ConnectionsList connectionsList={connections} />
+    </>
+  );
+};
 
 //mocks with dispatch
 const mockConnections = (): void => {
-    const dispatch = useDispatch();
-    
-    useEffect(() => {
-      let connectionsMocks = ConnectionsMock;
-      dispatch(initializeMockConnections(connectionsMocks));
-    }, []);
-}
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    let connectionsMocks = ConnectionsMock;
+    dispatch(initializeMockConnections(connectionsMocks));
+    console.log(connectionsMocks);
+  }, []);
+};
