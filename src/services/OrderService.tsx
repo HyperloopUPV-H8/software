f182bc4e0a498961018c9769700fe887ad543971
Sergio <smorsua@teleco.upv.es>
2022-12-01 17:20:00 +0100

    DEV-65 Minor changes to DataService & OrderService

--- src/services/OrderService.tsx
+++ src/services/OrderService.tsx
@@ -5,7 +5,7 @@ import { setOrders } from "@slices/ordersSlice";
 import { useDispatch } from "react-redux";
 import { createConnection } from "@models/Connection";
 import { updateConnection } from "@slices/connectionsSlice";
-
+import { mockOrderWebAdapters } from "@mocks/mockOrderDescriptions";
 interface IOrderService {
   sendOrder(order: Order): void;
 }
@@ -16,27 +16,48 @@ export const OrderServiceContext = createContext<IOrderService>(
 
 export const OrderService = ({ children }: any) => {
   const dispatch = useDispatch();
-  let orderSocket = useRef<WebSocket>();
+  const orderSocket = useRef<WebSocket>();
+  const orderService = useRef({
+    sendOrder(order: Order) {
+      orderSocket.current!.send(JSON.stringify(order));
+    },
+  });
 
   useEffect(() => {
+    fetchOrderDescriptions();
+    createOrderWebSocket();
+
+    return () => {
+      if (orderSocket.current) {
+        orderSocket.current.close();
+      }
+    };
+  }, []);
+
+  function fetchOrderDescriptions() {
     fetch(
       `http://${import.meta.env.VITE_SERVER_IP}:${
         import.meta.env.VITE_SERVER_PORT
       }${import.meta.env.VITE_ORDER_DESCRIPTIONS_URL}`
     )
-      .then((response) => response.json())
-      .then((orderWebAdapter: { [key: string]: OrderWebAdapter }) => {
-        let orders: OrderDescription[] = Object.values(orderWebAdapter).map(
+      .catch((reason) => {
+        console.error("Error fetching order descriptions", reason);
+      })
+      .then((response) => response!.json())
+      .catch((reason) =>
+        console.error("Error converting orderDescriptions to JSON", reason)
+      )
+      .then((orderWebAdapters: { [key: string]: OrderWebAdapter }) => {
+        let orders: OrderDescription[] = Object.values(orderWebAdapters).map(
           (adapter) => {
             return createOrderDescription(adapter);
           }
         );
         dispatch(setOrders(orders));
-      })
-      .catch((reason) => {
-        console.error(`Error fetching Orders Description: ${reason}`);
       });
+  }
 
+  function createOrderWebSocket() {
     orderSocket.current = new WebSocket(
       `ws://${import.meta.env.VITE_SERVER_IP}:${
         import.meta.env.VITE_SERVER_PORT
@@ -46,27 +67,17 @@ export const OrderService = ({ children }: any) => {
     orderSocket.current.onopen = () => {
       dispatch(updateConnection(createConnection("Orders", true)));
     };
+    orderSocket.current.onerror = () => {
+      //TODO: move all error handling to a separate file and make functions to create errors
+      console.error("Error in Order WebSocket");
+    };
     orderSocket.current.onclose = () => {
       dispatch(updateConnection(createConnection("Orders", false)));
     };
-
-    return () => {
-      if (orderSocket.current) {
-        orderSocket.current.close();
-      }
-    };
-  }, []);
-
-  let orderService = {
-    sendOrder(order: Order) {
-      //FIXME: could be undefined
-      console.log(order);
-      orderSocket.current!.send(JSON.stringify(order));
-    },
-  };
+  }
 
   return (
-    <OrderServiceContext.Provider value={orderService}>
+    <OrderServiceContext.Provider value={orderService.current}>
       {children}
     </OrderServiceContext.Provider>
   );
