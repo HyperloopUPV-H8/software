657d2111673857974db0a9ff976e4a4ccfd78432
lipezaballa <lipezaballa@gmail.com>
2023-01-09 22:01:05 +0100

    DEV-95: scroll in messageList
    The scroll goes to the end, but when you do a dropdown it stops. ResizeObserver is commented, but it has to be optimized yet

--- src/components/FaultsAndWarningLogger/MessageLogger/MessageList/MessageItem/MessageItem.tsx
+++ src/components/FaultsAndWarningLogger/MessageLogger/MessageList/MessageItem/MessageItem.tsx
@@ -6,80 +6,87 @@ import { getSofterHSLAColor, HSLAColor, hslaToString } from "@utils/color";
 import { Caret } from "@components/Caret/Caret";
 import { memo } from "react";
 interface Props {
-    message: Message;
-    color: HSLAColor;
+  message: Message;
+  color: HSLAColor;
+  onHandleScroll: () => void;
 }
 
-const MessageItem = ({ message, color }: Props) => {
-    const [isOverflowing, setIsOverflowing] = useState(true);
-    const [isOpen, setIsOpen] = useState(false);
+const MessageItem = ({ message, color, onHandleScroll }: Props) => {
+  const [isOverflowing, setIsOverflowing] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const wrapperRef = useRef<HTMLLIElement>(null);
-    const descriptionRef = useRef<HTMLDivElement>(null);
+  const wrapperRef = useRef<HTMLLIElement>(null);
+  const descriptionRef = useRef<HTMLDivElement>(null);
 
-    const resizeObserver = useRef(
-        new ResizeObserver((entries) => {
-            let descriptionElement = entries[0].target as HTMLElement;
-            setIsOverflowing(checkOverflow(descriptionElement));
-        })
-    );
+  //const isBottomLocked = useRef(true);
 
-    function checkOverflow(el: HTMLElement) {
-        el.style.whiteSpace = "nowrap";
-        let overflowing = el.clientWidth < el.scrollWidth;
-        el.style.whiteSpace = "";
+  //   function handleScrollChange() {
+  //     onHandleScroll();
+  //   }
 
-        return overflowing;
-    }
+  // const resizeObserver = useRef(
+  //     new ResizeObserver((entries) => {
+  //         let descriptionElement = entries[0].target as HTMLElement;
+  //         setIsOverflowing(checkOverflow(descriptionElement));
+  //     })
+  // );
 
-    useLayoutEffect(() => {
-        setIsOverflowing(checkOverflow(descriptionRef.current!));
-    }, []);
+  // function checkOverflow(el: HTMLElement) {
+  //     el.style.whiteSpace = "nowrap";
+  //     let overflowing = el.clientWidth < el.scrollWidth;
+  //     el.style.whiteSpace = "";
 
-    useEffect(() => {
-        resizeObserver.current.observe(descriptionRef.current!);
+  //     return overflowing;
+  // }
 
-        return () => {
-            resizeObserver.current.disconnect();
-        };
-    }, []);
+  // useLayoutEffect(() => {
+  //     setIsOverflowing(checkOverflow(descriptionRef.current!));
+  // }, []);
 
-    return (
-        <li
-            ref={wrapperRef}
-            className={`${styles.lineMsg} ${isOpen ? styles.open : ""}`}
-            key={message.id}
-            style={{
-                backgroundColor: hslaToString(getSofterHSLAColor(color, 48)),
-            }}
-        >
-            {isOverflowing && (
-                <Caret
-                    className={styles.caret}
-                    isOpen={isOpen}
-                    onClick={() => {
-                        setIsOpen((current) => !current);
-                    }}
-                />
-            )}
-            <div
-                className={styles.idMsg}
-                style={{ color: hslaToString(color) }}
-            >
-                {message.id + ":"}
-            </div>
-            <div
-                className={styles.description}
-                style={{ color: hslaToString(color) }}
-                ref={descriptionRef}
-            >
-                {message.description}
-            </div>
-            {message.count > 1 && (
-                <Counter className={styles.counter} count={message.count} />
-            )}
-        </li>
-    );
+  // useEffect(() => {
+  //     resizeObserver.current.observe(descriptionRef.current!);
+
+  //     return () => {
+  //         resizeObserver.current.disconnect();
+  //     };
+  // }, []);
+
+  return (
+    <li
+      ref={wrapperRef}
+      className={`${styles.lineMsg} ${isOpen ? styles.open : ""}`}
+      key={message.id}
+      style={{
+        backgroundColor: hslaToString(getSofterHSLAColor(color, 48)),
+      }}
+    >
+      {isOverflowing && (
+        <Caret
+          className={styles.caret}
+          isOpen={isOpen}
+          onClick={() => {
+            setIsOpen((current) => !current);
+            //isBottomLocked.current = !isBottomLocked.current;
+            //handleScrollChange();
+            onHandleScroll();
+          }}
+        />
+      )}
+      <div className={styles.idMsg} style={{ color: hslaToString(color) }}>
+        {message.id + ":"}
+      </div>
+      <div
+        className={styles.description}
+        style={{ color: hslaToString(color) }}
+        ref={descriptionRef}
+      >
+        {message.description}
+      </div>
+      {message.count > 1 && (
+        <Counter className={styles.counter} count={message.count} />
+      )}
+    </li>
+  );
 };
 
 export default memo(MessageItem);
--- src/components/FaultsAndWarningLogger/MessageLogger/MessageList/MessageList.tsx
+++ src/components/FaultsAndWarningLogger/MessageLogger/MessageList/MessageList.tsx
@@ -2,24 +2,45 @@ import MessageItem from "@components/FaultsAndWarningLogger/MessageLogger/Messag
 import styles from "@components/FaultsAndWarningLogger/MessageLogger/MessageList/MessageList.module.scss";
 import { Message } from "@models/Message";
 import { HSLAColor } from "@utils/color";
+import { useEffect, useRef } from "react";
 
 interface Props {
-    messages: Message[];
-    color: HSLAColor;
+  messages: Message[];
+  color: HSLAColor;
 }
 
 export const MessageList = ({ messages, color }: Props) => {
-    return (
-        <ul className={styles.messagesList}>
-            {messages.map((message) => {
-                return (
-                    <MessageItem
-                        key={message.listId}
-                        message={message}
-                        color={color}
-                    />
-                );
-            })}
-        </ul>
-    );
+  const isBottomLocked = useRef(true);
+  const emptyDivRef = useRef<HTMLDivElement>(null);
+
+  function handleScroll() {
+    isBottomLocked.current = !isBottomLocked.current;
+    console.log(isBottomLocked);
+  }
+
+  function scrollToBottom() {
+    emptyDivRef.current!.scrollIntoView({ behavior: "smooth" });
+  }
+
+  useEffect(() => {
+    if (isBottomLocked.current) {
+      scrollToBottom();
+    }
+  });
+
+  return (
+    <ul className={styles.messagesList}>
+      {messages.map((message) => {
+        return (
+          <MessageItem
+            key={message.listId}
+            message={message}
+            color={color}
+            onHandleScroll={handleScroll}
+          />
+        );
+      })}
+      <div className={styles.endDiv} ref={emptyDivRef} />
+    </ul>
+  );
 };
