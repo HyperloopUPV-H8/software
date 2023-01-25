78223033f36d72ca8523d02a537ecba88dda01d2
lipezaballa <lipezaballa@gmail.com>
2023-01-25 15:44:12 +0100

    DEV-95: Correcting PR comments

--- src/components/FaultsAndWarningLogger/MessageLogger/MessageList/MessageList.tsx
+++ src/components/FaultsAndWarningLogger/MessageLogger/MessageList/MessageList.tsx
@@ -6,43 +6,53 @@ import { useEffect, useRef, useState } from "react";
 import { useWheelScroll } from "./useWheelScroll";
 
 interface Props {
-  messages: Message[];
-  color: HSLAColor;
+    messages: Message[];
+    color: HSLAColor;
 }
 const MESSAGES_INITIALLY_SHOWN = 10;
 
 export const MessageList = ({ messages, color }: Props) => {
-  const scrollUlRef = useRef<HTMLUListElement>(null);
-  const isBottomLocked = useRef(true);
-  const [handleWheel] = useWheelScroll(scrollUlRef, messages, isBottomLocked);
-  const [messagesShown, setmessagesShown] = useState(MESSAGES_INITIALLY_SHOWN);
-  const firstMsgShown = useRef(0);
+    const scrollUlRef = useRef<HTMLUListElement>(null);
+    const isBottomLocked = useRef(true);
+    const handleWheel = useWheelScroll(scrollUlRef, messages, isBottomLocked);
+    const [messagesShown, setmessagesShown] = useState(
+        MESSAGES_INITIALLY_SHOWN
+    );
+    const firstMsgShown = useRef(0);
 
-  function handleButtonClick() {
-    setmessagesShown(messagesShown + MESSAGES_INITIALLY_SHOWN);
-    firstMsgShown.current -= MESSAGES_INITIALLY_SHOWN;
-  }
-
-  useEffect(() => {
-    if (isBottomLocked.current) {
-      firstMsgShown.current = messages.length - messagesShown;
+    function handleButtonClick() {
+        setmessagesShown(messagesShown + MESSAGES_INITIALLY_SHOWN);
+        firstMsgShown.current -= MESSAGES_INITIALLY_SHOWN;
     }
-  }, [messages]);
 
-  return (
-    <ul className={styles.messagesList} onWheel={handleWheel} ref={scrollUlRef}>
-      {messages.length > messagesShown ? (
-        <button className={styles.button} onClick={handleButtonClick}>
-          More msgs
-        </button>
-      ) : null}
-      {messages.map((message, index) => {
-        if (index >= firstMsgShown.current) {
-          return (
-            <MessageItem key={message.listId} message={message} color={color} />
-          );
+    useEffect(() => {
+        if (isBottomLocked.current) {
+            firstMsgShown.current = messages.length - messagesShown;
         }
-      })}
-    </ul>
-  );
+    }, [messages]);
+
+    return (
+        <ul
+            className={styles.messagesList}
+            onWheel={handleWheel}
+            ref={scrollUlRef}
+        >
+            {messages.length > messagesShown ? (
+                <button className={styles.button} onClick={handleButtonClick}>
+                    More msgs
+                </button>
+            ) : null}
+            {messages.map((message, index) => {
+                if (index >= firstMsgShown.current) {
+                    return (
+                        <MessageItem
+                            key={message.listId}
+                            message={message}
+                            color={color}
+                        />
+                    );
+                }
+            })}
+        </ul>
+    );
 };
--- src/components/FaultsAndWarningLogger/MessageLogger/MessageList/useWheelScroll.tsx
+++ src/components/FaultsAndWarningLogger/MessageLogger/MessageList/useWheelScroll.tsx
@@ -1,36 +1,37 @@
 import { Message } from "@models/Message";
 import { useEffect, WheelEvent } from "react";
 export const useWheelScroll = (
-  scrollUlRef: React.RefObject<HTMLUListElement>,
-  useEffectDeps: Message[],
-  isBottomLocked: React.MutableRefObject<boolean>
+    scrollUlRef: React.RefObject<HTMLUListElement>,
+    messages: Message[],
+    isBottomLocked: React.MutableRefObject<boolean>
 ) => {
-  function handleWheel(ev: WheelEvent) {
-    const bottom =
-      scrollUlRef.current!.scrollHeight - scrollUlRef.current!.scrollTop ===
-      scrollUlRef.current?.clientHeight;
+    function handleWheel(ev: WheelEvent) {
+        const bottom =
+            scrollUlRef.current!.scrollHeight -
+                scrollUlRef.current!.scrollTop ===
+            scrollUlRef.current?.clientHeight;
 
-    if (isBottomLocked.current) {
-      if (ev.deltaY < 0) {
-        isBottomLocked.current = false;
-      }
-    } else if (bottom && !isBottomLocked.current) {
-      isBottomLocked.current = true;
+        if (isBottomLocked.current) {
+            if (ev.deltaY < 0) {
+                isBottomLocked.current = false;
+            }
+        } else if (bottom && !isBottomLocked.current) {
+            isBottomLocked.current = true;
+        }
     }
-  }
 
-  function scrollToBottom() {
-    scrollUlRef.current?.scrollTo({
-      top: scrollUlRef.current.scrollHeight,
-      behavior: "smooth",
-    });
-  }
-
-  useEffect(() => {
-    if (isBottomLocked.current) {
-      scrollToBottom();
+    function scrollToBottom() {
+        scrollUlRef.current?.scrollTo({
+            top: scrollUlRef.current.scrollHeight,
+            behavior: "smooth",
+        });
     }
-  }, [useEffectDeps]);
 
-  return [handleWheel];
+    useEffect(() => {
+        if (isBottomLocked.current) {
+            scrollToBottom();
+        }
+    }, [messages]);
+
+    return handleWheel;
 };
