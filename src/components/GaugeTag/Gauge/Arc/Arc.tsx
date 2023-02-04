84c422487d2ae37cb7d24205f39d2ae8a4442005
Sergio <smorsua@teleco.upv.es>
2023-02-04 13:26:44 +0100

    Removed unnecesary group

--- src/components/GaugeTag/Gauge/Arc/Arc.tsx
+++ src/components/GaugeTag/Gauge/Arc/Arc.tsx
@@ -22,23 +22,20 @@ export const Arc = ({
     const dashArray = `${filledArc} ${circumference}`;
     const transform = `rotate(${-90 - sweep / 2}, ${radius}, ${radius})`;
     return (
-        <g
+        <circle
             viewBox={`0 0 ${radius * 2} ${radius * 2}`}
             className={className}
-        >
-            <circle
-                cx={radius}
-                cy={radius}
-                fill="transparent"
-                r={innerRadius}
-                strokeWidth={strokeWidth}
-                strokeDasharray={dashArray}
-                strokeLinecap={"round"}
-                transform={transform}
-                style={{
-                    transition: "stroke-dasharray 0.1s linear",
-                }}
-            />
-        </g>
+            cx={radius}
+            cy={radius}
+            fill="transparent"
+            r={innerRadius}
+            strokeWidth={strokeWidth}
+            strokeDasharray={dashArray}
+            strokeLinecap={"round"}
+            transform={transform}
+            style={{
+                transition: "stroke-dasharray 0.1s linear",
+            }}
+        />
     );
 };
