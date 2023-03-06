5b7c2bcdc5668e07e9d54d0c8fe8b85daf868348
Sergio <smorsua@teleco.upv.es>
2023-03-06 18:09:11 +0100

    Added different want views

--- src/components/AlertMailbox/FaultMessageView/ViolationView/WantView/WantView.tsx
+++ src/components/AlertMailbox/FaultMessageView/ViolationView/WantView/WantView.tsx
@@ -8,7 +8,10 @@ export const WantView = ({ violation, className = "" }: Props) => {
         <span className={`${className}`}>
             {violation.kind == "OUT_OF_BOUNDS" &&
                 `[${violation.want[0]}, ${violation.want[1]}]`}{" "}
-            {violation.kind != "OUT_OF_BOUNDS" && violation.want}
+            {violation.kind == "UPPER_BOUND" && `x < ${violation.want}`}
+            {violation.kind == "LOWER_BOUND" && `x > ${violation.want}`}
+            {violation.kind == "EQUALS" && violation.want}
+            {violation.kind == "NOT_EQUALS" && `not ${violation.want}`}
         </span>
     );
 };
