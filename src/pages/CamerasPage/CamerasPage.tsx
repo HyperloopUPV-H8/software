13b477b32e1be561840ea94d8f91291b821f8ee9
lipezaballa <lipezaballa@gmail.com>
2023-03-08 20:55:21 +0100

    DEV-15: Adding changes to Cameras Page

--- src/pages/CamerasPage/CamerasPage.tsx
+++ src/pages/CamerasPage/CamerasPage.tsx
@@ -5,13 +5,25 @@ export const CamerasPage = () => {
     return (
         <div className={styles.camerasContainer}>
             <div className={styles.camerasBody}>
-                <div className={styles.secondaryCameras}>
+                <div className={styles.overlayCameras}>
                     <div className={styles.title}>
-                        <div>Cam 1</div>
-                        <span className={styles.dot}></span>
+                        <div className={styles.name}>Cam 1</div>
+                        <div className={styles.dot}></div>
                     </div>
-                    <div className={styles.cam2}>Cam 2</div>
-                    <div className={styles.cam3}>Cam 3</div>
+                    <div className={styles.secondaryCameras}>
+                        <div className={styles.cam2}>
+                            <div className={styles.title}>
+                                <div className={styles.name}>Cam 2</div>
+                                <div className={styles.dot}></div>
+                            </div>
+                        </div>
+                        <div className={styles.cam3}>
+                            <div className={styles.title}>
+                                <div className={styles.name}>Cam 3</div>
+                                <div className={styles.dot}></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <CamerasFooter />
