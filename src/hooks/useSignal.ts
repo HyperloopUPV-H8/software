d4a5430f8c51ab983c5b4c370f584cf476cd3e88
Sergio <smorsua@teleco.upv.es>
2023-03-09 10:16:56 +0100

    Changes

--- src/hooks/useSignal.ts
+++ src/hooks/useSignal.ts
@@ -1,57 +1,70 @@
 import { useEffect, useRef, useState } from "react";
 
-export type Signal = {
-    event: "answer" | "offer" | "candidate" | "reject" | "close"
-    payload: string | RTCIceCandidateInit
-}
+export type Signal = SignalEvent | SignalCandidate;
 
-export function useSignal(addr: string, onSignal: (sig: Signal) => void): (sig: Signal) => boolean {
-    const websocket = useRef<WebSocket | null>()
+type SignalEvent = {
+    event: "answer" | "offer" | "reject" | "close";
+    payload: string;
+};
+
+type SignalCandidate = {
+    event: "candidate";
+    payload: RTCIceCandidateInit;
+};
+
+export function useSignal(
+    addr: string,
+    onSignal: (sig: Signal) => void
+): (sig: Signal) => boolean {
+    const websocket = useRef<WebSocket | null>();
 
     useEffect(() => {
-        connectSignal()
-    }, [])
+        connectSignal();
+    }, []);
+
+    function connectSignal() {
+        websocket.current = new WebSocket(addr);
+
+        websocket.current.onclose = (_) => {
+            console.log("closed signaling websocket");
+        };
+
+        websocket.current.onmessage = (ev) => {
+            const sig: Signal = JSON.parse(ev.data);
+
+            if (sig.event === "reject") {
+                console.log("last signal rejected, reason:", sig.payload);
+            } else if (sig.event === "close") {
+                closeSignal(sig.payload);
+            } else {
+                console.log("signal:", sig);
+                onSignal(sig);
+            }
+        };
+    }
 
     function sendSignal(signal: Signal): boolean {
         if (!websocket.current) {
-            return false
+            return false;
         }
 
-        websocket.current.send(JSON.stringify(signal))
+        websocket.current.send(JSON.stringify(signal));
 
-        return true
+        return true;
     }
 
     function closeSignal(reason: string) {
-        console.log("signal channel closed, reason:", reason)
+        console.log("signal channel closed, reason:", reason);
         if (!websocket.current) {
-            return
+            return;
         }
 
-        websocket.current.send(JSON.stringify({ event: "close", payload: "ok" }))
-        websocket.current.close()
-        websocket.current = null
+        websocket.current.send(
+            JSON.stringify({ event: "close", payload: "ok" })
+        );
+        websocket.current.close();
+        websocket.current = null;
     }
 
-    function connectSignal() {
-        const socket = new WebSocket(addr)
-        websocket.current = socket
-
-        socket.onclose = _ => closeSignal("")
-
-        socket.onmessage = ev => {
-            const sig: Signal = JSON.parse(ev.data)
-
-            if (sig.event === "reject" && typeof sig.payload === "string") {
-                console.log("last signal rejected, reason:", sig.payload)
-            } else if (sig.event === "close" && typeof sig.payload === "string") {
-                closeSignal(sig.payload)
-            } else {
-                console.log(sig)
-                onSignal(sig)
-            }
-        }
-    }
-
-    return sendSignal
+    return sendSignal;
 }
--- src/hooks/useWebRTC.ts
+++ src/hooks/useWebRTC.ts
@@ -1,74 +1,85 @@
 import { useEffect, useRef, useState } from "react";
 import { Signal, useSignal } from "./useSignal";
 
+export function useWebRTC(signalAddr: string, config?: RTCConfiguration) {
+    const [state, setState] = useState<RTCPeerConnectionState>("new");
+    const peer = useRef<RTCPeerConnection>();
+    const videoElement = useRef<HTMLVideoElement | null>(null);
+    const [stream, setStream] = useState<MediaStream>();
 
-
-export function useWebRTC(signalAddr: string, config?: RTCConfiguration): [RTCPeerConnectionState, React.MutableRefObject<HTMLVideoElement | null>] {
-    const [state, setState] = useState<RTCPeerConnectionState>("new")
-    let peer = useRef<RTCPeerConnection>()
-    const videoElement = useRef<HTMLVideoElement | null>(null)
+    useEffect(() => {
+        console.log("stream:", stream, "videoElement", videoElement.current);
+        if (stream && videoElement.current) {
+            console.log("set stream to video!");
+            videoElement.current.srcObject = stream;
+        }
+    }, [videoElement.current, stream]);
 
     const onSignal = async (lastSignal: Signal) => {
-        if (lastSignal.event === "offer" && typeof lastSignal.payload === "string") {
-            await onSignalOffer(lastSignal.payload)
-        } else if (lastSignal.event === "candidate" && lastSignal.payload instanceof RTCIceCandidate) {
-            onSignalCandidate(lastSignal.payload)
+        if (lastSignal.event === "offer") {
+            onSignalOffer(lastSignal.payload);
+        } else if (lastSignal.event === "candidate") {
+            onSignalCandidate(lastSignal.payload);
         }
-    }
-    const sendSignal = useSignal(signalAddr, onSignal)
+    };
 
+    const sendSignal = useSignal(signalAddr, onSignal);
 
     async function onSignalOffer(offer: string) {
-        peer.current = new RTCPeerConnection(config)
-        peer.current.onconnectionstatechange = _ => {
+        peer.current = new RTCPeerConnection(config);
+
+        peer.current.onconnectionstatechange = () => {
             if (!peer.current) {
-                return
+                return;
             }
 
-            setState(peer.current.connectionState)
-        }
+            setState(peer.current.connectionState);
+        };
 
-        peer.current.addTransceiver("video")
+        peer.current.addTransceiver("video");
 
-        await peer.current.setRemoteDescription({ type: "offer", sdp: offer })
+        await peer.current.setRemoteDescription({ type: "offer", sdp: offer });
 
-        const answer = await peer.current.createAnswer()
+        const answer = await peer.current.createAnswer();
 
         if (!answer.sdp) {
-            sendSignal({ event: "close", payload: "failed to create answer" })
-            return
+            sendSignal({ event: "close", payload: "failed to create answer" });
+            return;
         }
 
-        console.log("answer")
+        console.log("Created answer");
 
-        sendSignal({ event: "answer", payload: answer.sdp })
-        await peer.current.setLocalDescription(answer)
+        const wasAnwserSignalSent = sendSignal({
+            event: "answer",
+            payload: answer.sdp,
+        });
 
-        peer.current.onicecandidate = ev => {
+        console.log(wasAnwserSignalSent);
+        await peer.current.setLocalDescription(answer);
+
+        peer.current.onicecandidate = (ev) => {
             if (!ev.candidate) {
-                return
+                return;
             }
 
-            sendSignal({ event: "candidate", payload: ev.candidate.toJSON() })
-        }
+            sendSignal({ event: "candidate", payload: ev.candidate.toJSON() });
+        };
 
-        peer.current.ontrack = ev => {
-            while (!videoElement.current) {}
-
-            videoElement.current.srcObject = ev.streams[0]
-        }
+        peer.current.ontrack = (ev) => {
+            console.log("GOT THE TRACK");
+            setStream(ev.streams[0]);
+        };
     }
 
     async function onSignalCandidate(candidate: RTCIceCandidateInit) {
         if (!peer.current) {
-            sendSignal({ event: "reject", payload: "peer not ready" })
-            sendSignal({ event: "close", payload: "peer not ready" })
-            return
+            sendSignal({ event: "reject", payload: "peer not ready" });
+            sendSignal({ event: "close", payload: "peer not ready" });
+            return;
         }
 
-        peer.current.addIceCandidate(candidate)
+        peer.current.addIceCandidate(candidate);
     }
 
-
-    return [state, videoElement]
+    return [state, videoElement] as const;
 }
