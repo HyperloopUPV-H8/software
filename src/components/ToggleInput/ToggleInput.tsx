5fa9f66791e4d73f7311ef1ca0ed6c16d66c7c7c
jmaralo <martinezalonsojn@gmail.com>
2023-03-10 21:08:35 +0100

    DEV-14 add ToggleInput
    ToggleInput enables or disables an InputLabel based on a ToggleSwitch.

---
+++ src/components/ToggleInput/ToggleInput.tsx
@@ -0,0 +1,28 @@
+import { InputTag } from "components/InputTag/InputTag";
+import { ToggleSwitch } from "components/ToggleSwitch/ToggleSwitch";
+import { useToggle } from "hooks/useToggle";
+import { DetailedHTMLProps, InputHTMLAttributes, useEffect } from "react";
+import style from "./ToggleInput.module.scss";
+
+type Props = {
+    label: string,
+    onToggle?: (state: boolean) => void,
+} & DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+
+export function ToggleInput({onToggle, disabled, ...inputProps}: Props) {
+    const [isOn, flip] = useToggle();
+
+    if (onToggle) {
+        useEffect(() => {
+            onToggle(isOn)
+        }, [isOn])
+    }
+    
+
+    return (
+        <div className={style.toggleInputWrapper}>
+            <InputTag isOn={isOn} disabled={!isOn || disabled} {...inputProps} />
+            <ToggleSwitch isOn={isOn} flip={flip} />
+        </div>
+    )
+}
