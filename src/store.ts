9f42a5080a67131b00ed29a686b08e9b9e2de32f
Sergio <smorsua@teleco.upv.es>
2023-03-22 00:56:49 +0100

    Added measurements slice

--- src/store.ts
+++ src/store.ts
@@ -3,14 +3,17 @@ import podDataReducer from "slices/podDataSlice";
 import orderReducer from "slices/ordersSlice";
 import connectionsReducer from "slices/connectionsSlice";
 import messagesReducer from "slices/messagesSlice";
+import { measurementsReducer } from "slices/measurementsSlice";
 
 export const store = configureStore({
     reducer: {
         podData: podDataReducer,
+        measurements: measurementsReducer,
         orders: orderReducer,
         connections: connectionsReducer,
         messages: messagesReducer,
     },
+    middleware: [],
 });
 
 export type RootState = ReturnType<typeof store.getState>;
