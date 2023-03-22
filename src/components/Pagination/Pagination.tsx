14b544e3962343cf891c48722bbb5abf8adb9226
Sergio <smorsua@teleco.upv.es>
2023-03-22 14:23:16 +0100

    Added Pagination

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,35 @@
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import styles from "./Pagination.module.scss";
+import { PaginationItem } from "./PaginationItem/PaginationItem";
+
+type Props = {
+    routes: Array<string>;
+};
+
+export const Pagination = ({ routes }: Props) => {
+    const [activeRoute, setActiveRoute] = useState(routes[0]);
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        navigate(activeRoute);
+    }, [activeRoute]);
+
+    return (
+        <div className={styles.paginationWrapper}>
+            {routes.map((route, index) => {
+                return (
+                    <PaginationItem
+                        key={index}
+                        pageNumber={index}
+                        isActive={route == activeRoute}
+                        onClick={() => {
+                            setActiveRoute(route);
+                        }}
+                    />
+                );
+            })}
+        </div>
+    );
+};
