88d07a67f6356c4522391b8f146c83317588b1dc
Sergio <smorsua@teleco.upv.es>
2023-03-24 17:44:02 +0100

    Added useMediaStream hook

--- src/components/CameraContainer/Camera/Camera.tsx
+++ src/components/CameraContainer/Camera/Camera.tsx
@@ -1,5 +1,6 @@
 import styles from "./Camera.module.scss";
 import { useRef, useEffect } from "react";
+import { useMediaStream } from "./useMediaStream";
 
 type Props = {
     stream: MediaStream;
@@ -7,20 +8,12 @@ type Props = {
 };
 
 export const Camera = ({ stream, className = "" }: Props) => {
-    const videoRef = useRef<HTMLVideoElement>(null);
-
-    useEffect(() => {
-        if (videoRef.current) {
-            videoRef.current.srcObject = stream;
-        } else {
-            console.error("video ref is", videoRef.current);
-        }
-    }, [stream]);
+    const ref = useMediaStream(stream);
 
     return (
         <div className={`${styles.videoWrapper} ${className}`}>
             <video
-                ref={videoRef}
+                ref={ref}
                 muted
                 loop
                 playsInline
