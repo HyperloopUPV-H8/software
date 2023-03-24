b06a23c0f0f814bc95b8b249806fc0e490cfc2ac
Sergio <smorsua@teleco.upv.es>
2023-03-24 17:31:56 +0100

    Changes to useWebRTC hook

--- src/hooks/WebRTC/useWebRTC.ts
+++ src/hooks/WebRTC/useWebRTC.ts
@@ -2,69 +2,87 @@ import { useEffect, useRef, useState } from "react";
 import { SignalChannel } from "./SignalChannel";
 
 export function useWebRTC(signalUrl: string, configuration?: RTCConfiguration) {
-    const signalChannel = useRef<SignalChannel>(null!)
-    const videoElement = useRef<HTMLVideoElement>(null)
-    const peer = useRef<RTCPeerConnection>(null!)
-    const [peerState, setPeerState] = useState<RTCPeerConnectionState>("new")
+    const signalChannel = useRef<SignalChannel | null>(null); //TODO: arreglar lo de esta exclamacion rara
+    const peer = useRef<RTCPeerConnection | null>(null);
+    const [peerState, setPeerState] = useState<RTCPeerConnectionState>("new");
+    const [mediaStream, setMediaStream] = useState<MediaStream | null>(null);
 
     useEffect(() => {
-        peer.current = new RTCPeerConnection(configuration)
-        peer.current.addTransceiver("video")
-        peer.current.onconnectionstatechange = () => setPeerState(peer.current.connectionState)
-        peer.current.onicecandidate = handleCandidate
-        peer.current.ontrack = handleTrack
+        peer.current = new RTCPeerConnection(configuration);
+        peer.current.addTransceiver("video");
+        peer.current.onconnectionstatechange = () =>
+            setPeerState(peer.current!.connectionState);
+        peer.current.onicecandidate = handleCandidate;
+        peer.current.ontrack = handleTrack;
 
-        signalChannel.current = new SignalChannel(signalUrl)
-        signalChannel.current.addSignalListener("answer", handleAnswerSignal)
-        signalChannel.current.addSignalListener("candidate", handleCandidateSignal)
+        signalChannel.current = new SignalChannel(signalUrl);
+        signalChannel.current.addSignalListener("answer", handleAnswerSignal);
+        signalChannel.current.addSignalListener(
+            "candidate",
+            handleCandidateSignal
+        );
 
-        signalChannel.current.socket.onopen = () => startHandshake()
-    }, [])
+        signalChannel.current.socket.onopen = () => startHandshake();
+    }, []);
 
     function startHandshake() {
-        console.log("start handshake")
-        peer.current.createOffer().then(
+        console.log("start handshake");
+        peer.current!.createOffer().then(
             (offer: RTCSessionDescriptionInit) => sendAndUpdateOffer(offer),
-            (reason: Error) => signalChannel.current.sendClose(SignalCode.FailCreateOffer, reason.message)
-        )
+            (reason: Error) =>
+                signalChannel.current!.sendClose(
+                    SignalCode.FailCreateOffer,
+                    reason.message
+                )
+        );
     }
 
     function sendAndUpdateOffer(offer: RTCSessionDescriptionInit) {
-        peer.current.setLocalDescription(offer).then(
-            () => signalChannel.current.sendSignal("offer", offer),
-            (reason: Error) => signalChannel.current.sendClose(SignalCode.FailUpdateLocalOffer, reason.message)
-        )
+        peer.current!.setLocalDescription(offer).then(
+            () => signalChannel.current!.sendSignal("offer", offer),
+            (reason: Error) =>
+                signalChannel.current!.sendClose(
+                    SignalCode.FailUpdateLocalOffer,
+                    reason.message
+                )
+        );
     }
 
     function handleCandidate(ev: RTCPeerConnectionIceEvent) {
         if (!ev.candidate) {
-            return
+            return;
         }
 
-        signalChannel.current.sendSignal("candidate", ev.candidate.toJSON())
+        signalChannel.current!.sendSignal("candidate", ev.candidate.toJSON());
     }
 
     function handleTrack(ev: RTCTrackEvent) {
-        if (!videoElement.current) {
-            return
-        }
-
-        videoElement.current.srcObject = ev.streams[0]
+        setMediaStream(ev.streams[0]);
     }
 
     function handleCandidateSignal(signal: Signal<"candidate">) {
-        peer.current.addIceCandidate(signal.payload).then(
+        peer.current!.addIceCandidate(signal.payload).then(
             () => console.log("add candidate"),
-            (reason: Error) => signalChannel.current.sendError(SignalCode.FailAddCandidate, signal, reason.message)
-        )
+            (reason: Error) =>
+                signalChannel.current!.sendError(
+                    SignalCode.FailAddCandidate,
+                    signal,
+                    reason.message
+                )
+        );
     }
 
     function handleAnswerSignal(signal: Signal<"answer">) {
-        peer.current.setRemoteDescription(signal.payload).then(
+        peer.current!.setRemoteDescription(signal.payload).then(
             () => console.log("set answer"),
-            (reason: Error) => signalChannel.current.sendError(SignalCode.FailUpdateRemoteAnswer, signal, reason.message)
-        )
+            (reason: Error) =>
+                signalChannel.current!.sendError(
+                    SignalCode.FailUpdateRemoteAnswer,
+                    signal,
+                    reason.message
+                )
+        );
     }
 
-    return [videoElement, peerState] as const
+    return [mediaStream, peerState] as const;
 }
