c8b97f4ad15bbeef45112d0d92b848feb90a487f
lipezaballa <lipezaballa@gmail.com>
2023-03-27 10:28:29 +0200

    DEV-14: css changes

--- src/pages/TestingPage/TestingPage.tsx
+++ src/pages/TestingPage/TestingPage.tsx
@@ -3,8 +3,8 @@ import { PlayButton } from "components/PlayButton/PlayButton";
 import { ToggleButton } from "components/ToggleButton/ToggleButton";
 import { ToggleInput } from "components/ToggleInput/ToggleInput";
 import { PageWrapper } from "pages/PageWrapper/PageWrapper";
-import { FaPlane, FaPlaneArrival, FaPlaneDeparture } from "react-icons/fa"
-import style from "./TestingPage.module.scss"
+import { FaPlane, FaPlaneArrival, FaPlaneDeparture } from "react-icons/fa";
+import style from "./TestingPage.module.scss";
 
 export function TestingPage() {
     return (
@@ -15,25 +15,83 @@ export function TestingPage() {
                     <PlayButton variant="disabled" />
                 </div>
                 <div className={style.sectionWrapper}>
+                    <div className={style.title}>Controls</div>
                     <div className={style.controlsWrapper}>
-                        <ToggleButton label="levitation" icon={<FaPlaneDeparture />} />
+                        <ToggleButton
+                            label="levitation"
+                            icon={<FaPlaneDeparture />}
+                        />
                         <ToggleButton label="propulsion" icon={<FaPlane />} />
                         <ToggleButton label="brake" icon={<FaPlaneArrival />} />
                     </div>
                     <div className={style.inputWrapper}>
-                        <ToggleInput label="unit 0" type="number" min={-10.0} max={10.0} step={0.01} />
-                        <ToggleInput label="unit 1" type="number" min={-10.0} max={10.0} step={0.01} />
-                        <ToggleInput label="unit 2" type="number" min={-10.0} max={10.0} step={0.01} />
-                        <ToggleInput label="unit 3" type="number" min={-10.0} max={10.0} step={0.01} />
-                        <ToggleInput label="unit 4" type="number" min={-10.0} max={10.0} step={0.01} />
-                        <ToggleInput label="unit 5" type="number" min={-10.0} max={10.0} step={0.01} />
-                        <ToggleInput label="unit 6" type="number" min={-10.0} max={10.0} step={0.01} />
-                        <ToggleInput label="unit 7" type="number" min={-10.0} max={10.0} step={0.01} />
-                        <ToggleInput label="force x" type="number" min={-10.0} max={10.0} step={0.01} />
+                        <ToggleInput
+                            label="unit 0"
+                            type="number"
+                            min={-10.0}
+                            max={10.0}
+                            step={0.01}
+                        />
+                        <ToggleInput
+                            label="unit 1"
+                            type="number"
+                            min={-10.0}
+                            max={10.0}
+                            step={0.01}
+                        />
+                        <ToggleInput
+                            label="unit 2"
+                            type="number"
+                            min={-10.0}
+                            max={10.0}
+                            step={0.01}
+                        />
+                        <ToggleInput
+                            label="unit 3"
+                            type="number"
+                            min={-10.0}
+                            max={10.0}
+                            step={0.01}
+                        />
+                        <ToggleInput
+                            label="unit 4"
+                            type="number"
+                            min={-10.0}
+                            max={10.0}
+                            step={0.01}
+                        />
+                        <ToggleInput
+                            label="unit 5"
+                            type="number"
+                            min={-10.0}
+                            max={10.0}
+                            step={0.01}
+                        />
+                        <ToggleInput
+                            label="unit 6"
+                            type="number"
+                            min={-10.0}
+                            max={10.0}
+                            step={0.01}
+                        />
+                        <ToggleInput
+                            label="unit 7"
+                            type="number"
+                            min={-10.0}
+                            max={10.0}
+                            step={0.01}
+                        />
+                        <ToggleInput
+                            label="force x"
+                            type="number"
+                            min={-10.0}
+                            max={10.0}
+                            step={0.01}
+                        />
                     </div>
                     <ButtonTag label="Disturb" icon={<FaPlaneDeparture />} />
                 </div>
             </div>
-        </ PageWrapper>
-    )
+        </PageWrapper>
+    );
 }
