066e3c831140e23e0e8a1a54d1b973dcb61272f6
Sergio <smorsua@teleco.upv.es>
2023-04-16 19:55:15 +0200

    Small change to tabLayout

--- src/layouts/TabLayout/TabLayout.tsx
+++ src/layouts/TabLayout/TabLayout.tsx
@@ -30,7 +30,7 @@ export const TabLayout = ({ items }: Props) => {
                                 style={{
                                     display:
                                         visibleTab.id == item.id
-                                            ? "block"
+                                            ? "flex"
                                             : "none",
                                 }}
                             >
