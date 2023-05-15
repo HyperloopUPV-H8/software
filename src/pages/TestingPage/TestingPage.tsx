7c4b1768b0d2ab1a4b0dfabfc1acfdfb9b6767f9
lipezaballa <lipezaballa@gmail.com>
2023-05-15 11:50:16 +0200

    initiation to prepare websocket of HIL

--- src/pages/TestingPage/TestingPage.tsx
+++ src/pages/TestingPage/TestingPage.tsx
@@ -2,8 +2,59 @@ import { PageWrapper } from "pages/PageWrapper/PageWrapper";
 import style from "./TestingPage.module.scss";
 import { TestControls } from "./TestControls/TestControls";
 import { ThreeJsVehicle } from "./ThreeJs/ThreeJsVehicle";
+import { useEffect, useState } from "react";
+import { FormDescription } from "./TestControls/TestAttributes";
+
+const SERVER_URL = `${import.meta.env.VITE_SERVER_IP_HIL}:${
+    import.meta.env.VITE_SERVER_PORT_HIL
+}${import.meta.env.VITE_BACKEND_WEBSOCKET_PATH}`;
+
+const WEBSOCKET_URL = `ws://${SERVER_URL}`;
+
+export type VehicleState = {
+    xDistance: number;
+    current: number;
+    duty: number; //FIXME: Check it is a correct value
+    temperature: number;
+};
 
 export function TestingPage() {
+    const [socket, setSocket] = useState<WebSocket | null>(null);
+
+    const sendMessage = (controlForm: FormDescription) => {
+        const jsonFormDescription = JSON.stringify(controlForm);
+        socket!.send(jsonFormDescription);
+    }; //TODO: websocket as a prop to TestControl
+
+    useEffect(() => {
+        const newSocket = new WebSocket(WEBSOCKET_URL); //FIXME: It's correct?
+
+        newSocket.onopen = () => {
+            console.log("Open connection");
+        };
+
+        newSocket.onmessage = (event) => {
+            const json: VehicleState = JSON.parse(event.data); //TODO: This info to ThreeJsVehicle
+            console.log("Receive message: ", json);
+        };
+
+        newSocket.onclose = () => {
+            console.log("Conexión cerrada");
+        };
+
+        newSocket.onerror = (error) => {
+            console.error("WebSocket error: ", error);
+        };
+
+        // Actualizamos el estado del componente con la nueva instancia de WebSocket
+        setSocket(newSocket);
+
+        // TODO: Cerramos la conexión WebSocket al desmontar el componente, is it correct?
+        return () => {
+            newSocket.close();
+        };
+    }, []);
+
     return (
         <PageWrapper title="Testing">
             <div className={style.testingPageWrapper}>
@@ -14,9 +65,7 @@ export function TestingPage() {
                             <ThreeJsVehicle />
                         </div>
 
-                        <div className={style.info}>
-                            {/* <ThreeJsVehicle /> */}
-                        </div>
+                        <div className={style.info}></div>
                     </div>
                     <div className={style.graphics}></div>
                 </div>
