e3f51d875e8a89cbf2cc1527370e24f9897f1cef
smorsua <smorsua@teleco.upv.es>
2023-06-20 18:20:24 +0200

    Fixed extract function

--- lib/models/VehicleData/LevitationData.ts
+++ lib/models/VehicleData/LevitationData.ts
@@ -69,97 +69,97 @@ export type LevitationData = {
     control_state: EnumMeasurement;
 };
 
-const LPUId = "LPU";
+const LCUId = "LCU_MASTER";
 
 export function extractLevitationData(
     measurements: Measurements
 ): LevitationData {
     return {
-        general_state: measurements.boards[LPUId]["general_state"],
-        specific_state: measurements.boards[LPUId]["specific_state"],
-        slave_general_state: measurements.boards[LPUId]["slave_general_state"],
+        general_state: measurements.boards[LCUId]["general_state"],
+        specific_state: measurements.boards[LCUId]["specific_state"],
+        slave_general_state: measurements.boards[LCUId]["slave_general_state"],
         slave_specific_state:
-            measurements.boards[LPUId]["slave_specific_state"],
+            measurements.boards[LCUId]["slave_specific_state"],
 
-        airgap_1: measurements.boards[LPUId]["airgap_1"],
-        airgap_2: measurements.boards[LPUId]["airgap_2"],
-        airgap_3: measurements.boards[LPUId]["airgap_3"],
-        airgap_4: measurements.boards[LPUId]["airgap_4"],
-        slave_airgap_5: measurements.boards[LPUId]["slave_airgap_5"],
-        slave_airgap_6: measurements.boards[LPUId]["slave_airgap_6"],
-        slave_airgap_7: measurements.boards[LPUId]["slave_airgap_7"],
-        slave_airgap_8: measurements.boards[LPUId]["slave_airgap_8"],
+        airgap_1: measurements.boards[LCUId]["airgap_1"],
+        airgap_2: measurements.boards[LCUId]["airgap_2"],
+        airgap_3: measurements.boards[LCUId]["airgap_3"],
+        airgap_4: measurements.boards[LCUId]["airgap_4"],
+        slave_airgap_5: measurements.boards[LCUId]["slave_airgap_5"],
+        slave_airgap_6: measurements.boards[LCUId]["slave_airgap_6"],
+        slave_airgap_7: measurements.boards[LCUId]["slave_airgap_7"],
+        slave_airgap_8: measurements.boards[LCUId]["slave_airgap_8"],
 
-        current_coil_1: measurements.boards[LPUId]["current_coil_1"],
-        current_coil_2: measurements.boards[LPUId]["current_coil_2"],
-        current_coil_3: measurements.boards[LPUId]["current_coil_3"],
-        current_coil_4: measurements.boards[LPUId]["current_coil_4"],
+        current_coil_1: measurements.boards[LCUId]["current_coil_1"],
+        current_coil_2: measurements.boards[LCUId]["current_coil_2"],
+        current_coil_3: measurements.boards[LCUId]["current_coil_3"],
+        current_coil_4: measurements.boards[LCUId]["current_coil_4"],
         slave_current_coil_5:
-            measurements.boards[LPUId]["slave_current_coil_5"],
+            measurements.boards[LCUId]["slave_current_coil_5"],
         slave_current_coil_6:
-            measurements.boards[LPUId]["slave_current_coil_6"],
+            measurements.boards[LCUId]["slave_current_coil_6"],
         slave_current_coil_7:
-            measurements.boards[LPUId]["slave_current_coil_7"],
+            measurements.boards[LCUId]["slave_current_coil_7"],
         slave_current_coil_8:
-            measurements.boards[LPUId]["slave_current_coil_8"],
+            measurements.boards[LCUId]["slave_current_coil_8"],
 
-        temperature_coil_1: measurements.boards[LPUId]["temperature_coil_1"],
-        temperature_coil_2: measurements.boards[LPUId]["temperature_coil_2"],
-        temperature_coil_3: measurements.boards[LPUId]["temperature_coil_3"],
-        temperature_coil_4: measurements.boards[LPUId]["temperature_coil_4"],
+        temperature_coil_1: measurements.boards[LCUId]["temperature_coil_1"],
+        temperature_coil_2: measurements.boards[LCUId]["temperature_coil_2"],
+        temperature_coil_3: measurements.boards[LCUId]["temperature_coil_3"],
+        temperature_coil_4: measurements.boards[LCUId]["temperature_coil_4"],
         slave_temperature_coil_5:
-            measurements.boards[LPUId]["slave_temperature_coil_5"],
+            measurements.boards[LCUId]["slave_temperature_coil_5"],
         slave_temperature_coil_6:
-            measurements.boards[LPUId]["slave_temperature_coil_6"],
+            measurements.boards[LCUId]["slave_temperature_coil_6"],
         slave_temperature_coil_7:
-            measurements.boards[LPUId]["slave_temperature_coil_7"],
+            measurements.boards[LCUId]["slave_temperature_coil_7"],
         slave_temperature_coil_8:
-            measurements.boards[LPUId]["slave_temperature_coil_8"],
+            measurements.boards[LCUId]["slave_temperature_coil_8"],
 
-        temperature_lpu_1: measurements.boards[LPUId]["temperature_lpu_1"],
-        temperature_lpu_2: measurements.boards[LPUId]["temperature_lpu_2"],
-        temperature_lpu_3: measurements.boards[LPUId]["temperature_lpu_3"],
-        temperature_lpu_4: measurements.boards[LPUId]["temperature_lpu_4"],
+        temperature_lpu_1: measurements.boards[LCUId]["temperature_lpu_1"],
+        temperature_lpu_2: measurements.boards[LCUId]["temperature_lpu_2"],
+        temperature_lpu_3: measurements.boards[LCUId]["temperature_lpu_3"],
+        temperature_lpu_4: measurements.boards[LCUId]["temperature_lpu_4"],
         slave_temperature_lpu_5:
-            measurements.boards[LPUId]["slave_temperature_lpu_5"],
+            measurements.boards[LCUId]["slave_temperature_lpu_5"],
         slave_temperature_lpu_6:
-            measurements.boards[LPUId]["slave_temperature_lpu_6"],
+            measurements.boards[LCUId]["slave_temperature_lpu_6"],
         slave_temperature_lpu_7:
-            measurements.boards[LPUId]["slave_temperature_lpu_7"],
+            measurements.boards[LCUId]["slave_temperature_lpu_7"],
         slave_temperature_lpu_8:
-            measurements.boards[LPUId]["slave_temperature_lpu_8"],
+            measurements.boards[LCUId]["slave_temperature_lpu_8"],
 
-        battery_voltage_1: measurements.boards[LPUId]["battery_voltage_1"],
-        battery_voltage_2: measurements.boards[LPUId]["battery_voltage_2"],
+        battery_voltage_1: measurements.boards[LCUId]["battery_voltage_1"],
+        battery_voltage_2: measurements.boards[LCUId]["battery_voltage_2"],
         slave_battery_voltage_3:
-            measurements.boards[LPUId]["slave_battery_voltage_3"],
+            measurements.boards[LCUId]["slave_battery_voltage_3"],
         slave_battery_voltage_4:
-            measurements.boards[LPUId]["slave_battery_voltage_4"],
+            measurements.boards[LCUId]["slave_battery_voltage_4"],
 
-        battery_current_1: measurements.boards[LPUId]["battery_current_1"],
-        battery_current_2: measurements.boards[LPUId]["battery_current_2"],
+        battery_current_1: measurements.boards[LCUId]["battery_current_1"],
+        battery_current_2: measurements.boards[LCUId]["battery_current_2"],
         slave_battery_current_3:
-            measurements.boards[LPUId]["slave_battery_current_3"],
+            measurements.boards[LCUId]["slave_battery_current_3"],
         slave_battery_current_4:
-            measurements.boards[LPUId]["slave_battery_current_4"],
+            measurements.boards[LCUId]["slave_battery_current_4"],
 
-        reference_current_1: measurements.boards[LPUId]["reference_current_1"],
-        reference_current_2: measurements.boards[LPUId]["reference_current_2"],
-        reference_current_3: measurements.boards[LPUId]["reference_current_3"],
-        reference_current_4: measurements.boards[LPUId]["reference_current_4"],
+        reference_current_1: measurements.boards[LCUId]["reference_current_1"],
+        reference_current_2: measurements.boards[LCUId]["reference_current_2"],
+        reference_current_3: measurements.boards[LCUId]["reference_current_3"],
+        reference_current_4: measurements.boards[LCUId]["reference_current_4"],
         slave_reference_current_5:
-            measurements.boards[LPUId]["slave_reference_current_5"],
+            measurements.boards[LCUId]["slave_reference_current_5"],
         slave_reference_current_6:
-            measurements.boards[LPUId]["slave_reference_current_6"],
+            measurements.boards[LCUId]["slave_reference_current_6"],
         slave_reference_current_7:
-            measurements.boards[LPUId]["slave_reference_current_7"],
+            measurements.boards[LCUId]["slave_reference_current_7"],
         slave_reference_current_8:
-            measurements.boards[LPUId]["slave_reference_current_8"],
+            measurements.boards[LCUId]["slave_reference_current_8"],
 
-        rot_x: measurements.boards[LPUId]["rot_x"],
-        rot_y: measurements.boards[LPUId]["rot_y"],
-        rot_z: measurements.boards[LPUId]["rot_z"],
+        rot_x: measurements.boards[LCUId]["rot_x"],
+        rot_y: measurements.boards[LCUId]["rot_y"],
+        rot_z: measurements.boards[LCUId]["rot_z"],
 
-        control_state: measurements.boards[LPUId]["control_state"],
+        control_state: measurements.boards[LCUId]["control_state"],
     } as LevitationData;
 }
