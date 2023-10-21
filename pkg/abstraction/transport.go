999b06a8084f70a77bdadbbec93ccaa5c3424dd4
Juan Martinez <martinezalonsojn@gmail.com>
2023-10-21 22:49:34 +0200

    Add transport documentation

--- pkg/abstraction/transport.go
+++ pkg/abstraction/transport.go
@@ -1,22 +1,34 @@
 package abstraction
 
+// TransportEvent is an event of the Transport module
 type TransportEvent string
 
+// TransportTarget specifies who should get certain information
 type TransportTarget uint16
 
+// TransportNotification is a notification of an event being received
 type TransportNotification interface {
 	Event() TransportEvent
 }
 
+// TransportMessage is a request for data to be sent to the vehicle
 type TransportMessage interface {
 	Event() TransportEvent
 }
 
+// Transport is the module in charge of handling communication specifics with the
+// vehicle
 type Transport interface {
+	// SendMessage sends the provided message to the vehicle
 	SendMessage(TransportMessage) error
+
+	// SetAPI sets the API the Transport must use to communicate with the rest of the back-end
+	// through the Vehicle. It should avoid using any other means
 	SetAPI(TransportAPI)
 }
 
+// TransportAPI is the available API for the Transport module
 type TransportAPI interface {
+	// Notification notifies the back-end that an event has been received
 	Notification(TransportNotification)
 }
