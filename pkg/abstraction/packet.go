9b0411e73061e5770129d8aaef8249b252f27613
Juan Martinez <martinezalonsojn@gmail.com>
2023-10-21 22:46:31 +0200

    Add Packet docs

--- pkg/abstraction/packet.go
+++ pkg/abstraction/packet.go
@@ -1,6 +1,9 @@
 package abstraction
 
+// PacketId is the unique id of a packet
 type PacketId uint16
+
+// Packet is the abstraction of information sent or received from the vehicle
 type Packet interface {
 	Id() PacketId
 }
