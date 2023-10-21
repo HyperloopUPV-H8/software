d9145c21430ae4e51c6955663a9841cd726d207c
Juan Martinez <martinezalonsojn@gmail.com>
2023-10-21 23:23:29 +0200

    Add fields to transport

--- pkg/transport/transport.go
+++ pkg/transport/transport.go
@@ -1,8 +1,23 @@
 package transport
 
-import "github.com/HyperloopUPV-H8/h9-backend/pkg/abstraction"
+import (
+	"github.com/HyperloopUPV-H8/h9-backend/pkg/abstraction"
+	"github.com/HyperloopUPV-H8/h9-backend/pkg/transport/network"
+	"github.com/HyperloopUPV-H8/h9-backend/pkg/transport/presentation"
+	"github.com/HyperloopUPV-H8/h9-backend/pkg/transport/session"
+)
 
 type Transport struct {
+	decoder *presentation.PacketDecoder
+	encoder *presentation.PacketEncoder
+
+	conversations map[network.Socket]*session.SocketBuffer
+
+	sniffer *network.Sniffer
+	boards  map[abstraction.BoardId]*network.TCPConn
+
+	tftp *network.TFTPConn
+
 	api abstraction.TransportAPI
 }
 
