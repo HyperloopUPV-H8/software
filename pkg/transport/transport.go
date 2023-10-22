f083ef5be8615709902203dcd24786444a494041
Juan Martinez <martinezalonsojn@gmail.com>
2023-10-22 17:08:56 +0200

    Add transport documentation

--- pkg/transport/transport.go
+++ pkg/transport/transport.go
@@ -7,6 +7,12 @@ import (
 	"github.com/HyperloopUPV-H8/h9-backend/pkg/transport/session"
 )
 
+// Transport is the module in charge of handling network communication with
+// the vehicle.
+//
+// It uses events to differentiate different kinds of messages. Each message
+// or notification has an associated event which is used to determine the
+// action to take.
 type Transport struct {
 	decoder *presentation.PacketDecoder
 	encoder *presentation.PacketEncoder
@@ -21,6 +27,8 @@ type Transport struct {
 	api abstraction.TransportAPI
 }
 
+// SendMessage triggers an event to send something to the vehicle. Some messages
+// might additional means to pass information around (e.g. file read and write)
 func (transport *Transport) SendMessage(message abstraction.TransportMessage) error {
 	switch msg := message.(type) {
 	case PacketMessage:
@@ -46,6 +54,7 @@ func (transport *Transport) handleFileRead(message FileReadMessage) error {
 	panic("TODO!")
 }
 
+// SetAPI sets the API that the Transport will use
 func (transport *Transport) SetAPI(api abstraction.TransportAPI) {
 	transport.api = api
 	// TODO: make decoder use the api Notify method
