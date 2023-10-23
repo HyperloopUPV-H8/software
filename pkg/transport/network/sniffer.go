289749dbbc7e0459c5d37479e4ac3b2ca3096406
Juan Martinez <martinezalonsojn@gmail.com>
2023-10-23 12:43:55 +0200

    Add Decoder basic definition

--- pkg/transport/network/sniffer.go
+++ pkg/transport/network/sniffer.go
@@ -1,5 +1,48 @@
 package network
 
+import (
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+)
+
+type Decoder struct {
+	eth  layers.Ethernet
+	ipv4 layers.IPv4
+	tcp  layers.TCP
+	udp  layers.UDP
+
+	parser *gopacket.DecodingLayerParser
+}
+
+func NewDecoder(first gopacket.LayerType) Decoder {
+	decoder := Decoder{}
+	decoder.parser = gopacket.NewDecodingLayerParser(first, &decoder.eth, &decoder.ipv4, &decoder.tcp, &decoder.udp)
+	return decoder
+
+}
+
+func (decoder *Decoder) Decode(data []byte) ([]gopacket.LayerType, error) {
+	decoded := []gopacket.LayerType{}
+	err := decoder.parser.DecodeLayers(data, &decoded)
+	return decoded, err
+}
+
+func (decoder *Decoder) IPv4() layers.IPv4 {
+	return decoder.ipv4
+}
+
+func (decoder *Decoder) TCP() layers.TCP {
+	return decoder.tcp
+}
+
+func (decoder *Decoder) UDP() layers.UDP {
+	return decoder.udp
+}
+
+func (decoder *Decoder) Eth() layers.Ethernet {
+	return decoder.eth
+}
+
 type Socket struct {
 	LocalIP    string
 	LocalPort  uint16
@@ -7,4 +50,5 @@ type Socket struct {
 	RemotePort uint16
 }
 
-type Sniffer struct{}
+type Sniffer struct {
+}
