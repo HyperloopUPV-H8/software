4a3ee756fce20e54d2dffa27a00daec7a1960cc8
Juan Martinez <martinezalonsojn@gmail.com>
2023-10-23 15:32:19 +0200

    Relocate decoder within sniffer file

--- pkg/transport/network/sniffer.go
+++ pkg/transport/network/sniffer.go
@@ -8,51 +8,6 @@ import (
 	"github.com/google/gopacket/pcap"
 )
 
-type Decoder struct {
-	eth     layers.Ethernet
-	ipv4    layers.IPv4
-	ipipv4  layers.IPv4
-	tcp     layers.TCP
-	udp     layers.UDP
-	payload gopacket.Payload
-
-	parser *gopacket.DecodingLayerParser
-}
-
-func NewDecoder(first gopacket.LayerType) Decoder {
-	dec := new(Decoder)
-	dec.parser = gopacket.NewDecodingLayerParser(first, &dec.eth, &dec.ipv4, &dec.ipipv4, &dec.tcp, &dec.udp, &dec.payload)
-	dec.parser.IgnoreUnsupported = true
-	return *dec
-
-}
-
-func (decoder *Decoder) Decode(data []byte) ([]gopacket.LayerType, error) {
-	decoded := []gopacket.LayerType{}
-	err := decoder.parser.DecodeLayers(data, &decoded)
-	return decoded, err
-}
-
-func (decoder *Decoder) Eth() layers.Ethernet {
-	return decoder.eth
-}
-
-func (decoder *Decoder) IPv4() layers.IPv4 {
-	return decoder.ipipv4
-}
-
-func (decoder *Decoder) TCP() layers.TCP {
-	return decoder.tcp
-}
-
-func (decoder *Decoder) UDP() layers.UDP {
-	return decoder.udp
-}
-
-func (decoder *Decoder) Payload() []byte {
-	return decoder.payload
-}
-
 type Socket struct {
 	SrcIP   net.IP
 	SrcPort uint16
@@ -110,4 +65,49 @@ layerLoop:
 	}
 
 	return socket, sniffer.decoder.Payload(), nil
+}
+
+type Decoder struct {
+	eth     layers.Ethernet
+	ipv4    layers.IPv4
+	ipipv4  layers.IPv4
+	tcp     layers.TCP
+	udp     layers.UDP
+	payload gopacket.Payload
+
+	parser *gopacket.DecodingLayerParser
+}
+
+func NewDecoder(first gopacket.LayerType) Decoder {
+	dec := new(Decoder)
+	dec.parser = gopacket.NewDecodingLayerParser(first, &dec.eth, &dec.ipv4, &dec.ipipv4, &dec.tcp, &dec.udp, &dec.payload)
+	dec.parser.IgnoreUnsupported = true
+	return *dec
+
+}
+
+func (decoder *Decoder) Decode(data []byte) ([]gopacket.LayerType, error) {
+	decoded := []gopacket.LayerType{}
+	err := decoder.parser.DecodeLayers(data, &decoded)
+	return decoded, err
+}
+
+func (decoder *Decoder) Eth() layers.Ethernet {
+	return decoder.eth
+}
+
+func (decoder *Decoder) IPv4() layers.IPv4 {
+	return decoder.ipipv4
+}
+
+func (decoder *Decoder) TCP() layers.TCP {
+	return decoder.tcp
+}
+
+func (decoder *Decoder) UDP() layers.UDP {
+	return decoder.udp
+}
+
+func (decoder *Decoder) Payload() []byte {
+	return decoder.payload
 }
