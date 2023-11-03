5b7a15cd0cd98d2f53ac0f7d7c0265dfc0e86617
Juan Martinez <martinezalonsojn@gmail.com>
2023-11-03 15:09:58 +0100

    Add tftp client read and write logic

--- pkg/transport/network/tftp/client.go
+++ pkg/transport/network/tftp/client.go
@@ -1,14 +1,39 @@
 package tftp
 
-import "io"
+import (
+	"io"
+
+	"github.com/pin/tftp/v3"
+)
 
 type Client struct {
+	mode       string
+	conn       *tftp.Client
+	onProgress progressCallback
 }
 
-func (client *Client) Read(filename string, output io.Writer) error {
-	panic("TODO!")
+func (client *Client) ReadFile(filename string, output io.Writer) (int64, error) {
+	recv, err := client.conn.Receive(filename, client.mode)
+	if err != nil {
+		return 0, err
+	}
+
+	writer := newProgressWriter(filename, client.onProgress, output)
+
+	n, err := recv.WriteTo(writer)
+
+	return n, err
 }
 
-func (client *Client) Write(filename string, input io.Reader) error {
-	panic("TODO!")
+func (client *Client) WriteFile(filename string, input io.Reader) (int64, error) {
+	send, err := client.conn.Send(filename, client.mode)
+	if err != nil {
+		return 0, err
+	}
+
+	reader := newProgressReader(filename, client.onProgress, input)
+
+	n, err := send.ReadFrom(reader)
+
+	return n, err
 }
