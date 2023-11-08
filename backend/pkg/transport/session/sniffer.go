c892524d41867dae1c0e8ef0170cdf4c2a362b04
Juan Martinez <martinezalonsojn@gmail.com>
2023-11-08 16:29:11 +0100

    Add documentation

--- backend/pkg/transport/session/sniffer.go
+++ backend/pkg/transport/session/sniffer.go
@@ -7,17 +7,23 @@ import (
 	"github.com/HyperloopUPV-H8/h9-backend/pkg/transport/network"
 )
 
+// conversationCallback is called when a new conversation is detected, the conversation socket,
+// as well as the reader to get its packets, is provided through this callback.
 type conversationCallback = func(socket network.Socket, reader io.Reader)
 
-type PacketReader interface {
+// packetReader is a reader that generates packets from network connections
+type packetReader interface {
 	ReadNext() (network.Socket, []byte, error)
 }
 
+// Sniffer demux is a helper that takes all packets returned by the Sniffer and splits their
+// contents onto buffers based on the source and destination IPs and ports
 type SnifferDemux struct {
 	onConversation conversationCallback
 	conversations  map[network.Socket]*bytes.Buffer
 }
 
+// NewSnifferDemux creates a new SnifferDemux with the provided onConversation callback
 func NewSnifferDemux(onConversation conversationCallback) *SnifferDemux {
 	return &SnifferDemux{
 		conversations:  make(map[network.Socket]*bytes.Buffer),
@@ -25,7 +31,11 @@ func NewSnifferDemux(onConversation conversationCallback) *SnifferDemux {
 	}
 }
 
-func (demux *SnifferDemux) ReadPackets(reader PacketReader) error {
+// ReadPackets consumes the provided PacketReader.
+//
+// ReadPackets will block until an error is returned, callers are advised to run this
+// in a goroutine.
+func (demux *SnifferDemux) ReadPackets(reader packetReader) error {
 	for {
 		socket, data, err := reader.ReadNext()
 		if err != nil {
