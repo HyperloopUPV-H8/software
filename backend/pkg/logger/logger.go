aca5c1b85b7b54a4a87c388337633fbab0e1d6b2
[Marc Sanchis] <[markix95@gmail.com]>
2023-11-08 19:12:15 +0100

    Adaptation to abstraction

--- backend/pkg/logger/logger.go
+++ backend/pkg/logger/logger.go
@@ -1,112 +1,81 @@
 /*
+v0.2.0
 The logger package is responsible for logging the data received from the
 pod and also for reaching that data later on
 
-As for 2023-11-08 the logger is in need of the implementation of handling
-functions or a logger_handler file.
-The logger needs an implementation of a "file" interface.
+As of 2023-11-08 the logger is equiped with the main functions and is ready
+to be used. However, the logger is not yet integrated with the rest of the
+system.
 */
-
 package logger
 
 import (
+	"errors"
 	"time"
-
-	"github.com/HyperloopUPV-H8/h9-backend/internal/logger_handler"
-	"github.com/rs/zerolog"
-	trace "github.com/rs/zerolog/log"
 )
 
-type Logger struct {
-	ID            string // common.Set[string] or other similar resolution
-	file          string
-	flushInterval time.Duration
-	trace         zerolog.Logger
+type LoggerName string
+
+type DataRecord struct {
+	Timestamp time.Time
+	Data      string
+	logger    LoggerName
 }
 
-type Config struct {
-	Filename      string `toml:"filename"`
-	FlushInterval string `toml:"flush_interval"`
+func (dr *DataRecord) Name() LoggerName {
+	return dr.logger
 }
 
-/*
-TODO!
-type file interface {
-	Flush()
-	Write(data []byte)
-	Has(id string) bool
-	Close()
+type DataRequest struct {
+	Timestamp time.Time
+	Data      string
+	logger    LoggerName
 }
 
-/*
-TODO!
-type LogType interface {
-	Type() string
+func (dr *DataRequest) Name() LoggerName {
+	return dr.logger
 }
-*/
 
-func NewLogger(name string, id string, config Config) Logger {
-	trace := trace.With().Str("component", name).Logger()
+type LoggerFunc func(*DataRecord) error
+type RequestFunc func(*DataRequest) (*DataRecord, error)
 
-	flushInterval, err := time.ParseDuration(config.FlushInterval)
-	if err != nil {
-		trace.Fatal().Err(err).Msg("error parsing flush interval")
+type Logger interface {
+	PushRecord(*DataRecord) error
+	PullRecord(*DataRequest) (*DataRecord, error)
+}
+
+type BaseLogger struct {
+	logFuncs     map[LoggerName]LoggerFunc
+	requestFuncs map[LoggerName]RequestFunc
+}
+
+func NewBaseLogger() *BaseLogger {
+	return &BaseLogger{
+		logFuncs: map[LoggerName]LoggerFunc{
+			"datalogger": DataLog,
+			"msglogger":  MsgLog,
+			"ordlogger":  OrdLog,
+			"suslogger":  SusLog,
+		},
+		requestFuncs: map[LoggerName]RequestFunc{
+			"datalogger": DataRequest,
+			"msglogger":  MsgRequest,
+			"ordlogger":  OrdRequest,
+			"suslogger":  SusRequest,
+		},
 	}
+}
 
-	return Logger{
-		ID:            id,
-		file:          config.Filename,
-		flushInterval: flushInterval,
-		trace:         trace,
+func (bl *BaseLogger) PushRecord(record *DataRecord) error {
+	if logFunc, ok := bl.logFuncs[record.Name()]; ok {
+		return logFunc(record)
 	}
+	return errors.New("unknown logger name")
 }
 
-func (l *Logger) Start(basePath string) {
-	go func() {
-		for {
-			time.Sleep(l.flushInterval)
-			l.FlushRoutine()
-		}
-	}()
-}
-
-func (l *Logger) StartLogger(data string, basePath string, file string) {
-	file := l.createFile(basePath)
-	flushTicker := time.NewTicker(l.flushInterval)
-	done := make(chan struct{})
-	go l.FlushRoutine(flushTicker.C, file, done)
-
-	for data := range data {
-		if l.ID.Has(data.ID()) {
-			file.Write(data)
-		}
+func (bl *BaseLogger) PullRecord(request *DataRequest) (*DataRecord, error) {
+	if requestFunc, ok := bl.requestFuncs[request.Name()]; ok {
+		return requestFunc(request)
 	}
-
-	done <- struct{}{}
-	flushTicker.Stop()
-	file.Close()
-}
-
-func (l *Logger) StopLogger(done chan struct{}) {
-	done <- struct{}{}
-}
-
-func (l *Logger) createFile(basePath string) logger_handler.CSVFile {
-	file, err := logger_handler.NewCSVFile(basePath, fl.fileName)
-	if err != nil {
-		l.trace.Fatal().Err(err).Msg("error creating file")
-	}
-	return file
-}
-
-func (l *Logger) FlushRoutine(tickerChan <-chan time.Time, file string, done chan struct{}) {
-loop:
-	for {
-		select {
-		case <-tickerChan:
-			file.Flush()
-		case <-done:
-			break loop
-		}
-	}
+	return nil, errors.New("unknown logger name")
 }
