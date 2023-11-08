b6aab845c8a96a7e3b7f23e2a052c5b69783337c
Juan Martinez <martinezalonsojn@gmail.com>
2023-11-08 16:17:37 +0100

    Add SnifferDemux ReadPackets method

--- backend/pkg/transport/session/sniffer.go
+++ backend/pkg/transport/session/sniffer.go
@@ -2,13 +2,34 @@ package session
 
 import "github.com/HyperloopUPV-H8/h9-backend/pkg/transport/network"
 
-type conversationCallback = func(socket network.Socket, reader SocketReader)
+type conversationCallback = func(socket network.Socket, reader *SocketReader)
 
 type PacketReader interface {
 	ReadNext() (network.Socket, []byte, error)
 }
 
 type SnifferDemux struct {
-	source         PacketReader
 	onConversation conversationCallback
+	conversations  map[network.Socket]*SocketReader
+}
+
+func (demux *SnifferDemux) ReadPackets(reader PacketReader) error {
+	for {
+		socket, data, err := reader.ReadNext()
+		if err != nil {
+			return err
+		}
+
+		conversation, ok := demux.conversations[socket]
+		if !ok {
+			demux.conversations[socket] = newConversation()
+			conversation = demux.conversations[socket]
+			demux.onConversation(socket, conversation)
+		}
+
+		_, err = conversation.Write(data)
+		if err != nil {
+			return err
+		}
+	}
 }
--- backend/pkg/transport/session/socket.go
+++ backend/pkg/transport/session/socket.go
@@ -2,3 +2,11 @@ package session
 
 type SocketReader struct {
 }
+
+func newConversation() *SocketReader {
+	panic("TODO!")
+}
+
+func (reader *SocketReader) Write(p []byte) (n int, err error) {
+	panic("TODO!")
+}
