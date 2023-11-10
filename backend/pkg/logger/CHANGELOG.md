e4eaa7d1a5401504e002f0901baed06653e5ca8f
[Marc Sanchis] <[markix95@gmail.com]>
2023-11-10 19:33:46 +0100

    logger-structure-v2

---
+++ backend/pkg/logger/CHANGELOG.md
@@ -0,0 +1,5 @@
+# logger-structure
+## Commit 5 (v0.3.0)
+- Create ```CHANGELOG.md``` file
+- Implement the logger abstraction
+- Implement new Start() and Stop() functions
--- backend/pkg/logger/logger.go
+++ backend/pkg/logger/logger.go
@@ -1,71 +1,119 @@
-/*
-v0.2.0
-The logger package is responsible for logging the data received from the
-pod and also for reaching that data later on
-
-As of 2023-11-08 the logger is equiped with the main functions and is ready
-to be used. However, the logger is not yet integrated with the rest of the
-system.
-*/
 package logger
 
 import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"sync"
 	"time"
 
-	"github.com/HyperloopUPV-H8/h9-backend/pkg/logger/data"
-	"github.com/HyperloopUPV-H8/h9-backend/pkg/logger/msg"
-	"github.com/HyperloopUPV-H8/h9-backend/pkg/logger/ord"
-	"github.com/HyperloopUPV-H8/h9-backend/pkg/logger/state"
+	"github.com/HyperloopUPV-H8/h9-backend/pkg/abstraction"
 )
 
-type LoggerName string
+var _ abstraction.Logger = &Logger{}
 
-type DataL struct {
-	Timestamp string
-	Data      string
-	Logger    LoggerName
-}
-
-func (dl *DataL) Name() LoggerName {
-	return dl.Logger
-}
-
-type DataR struct {
+type Record struct {
+	NameValue abstraction.LoggerName
+	Data      interface{}
 	Timestamp time.Time
-	Data      string
-	Logger    LoggerName
 }
 
-func (dr *DataR) Name() LoggerName {
-	return dr.Logger
+func (r *Record) Name() abstraction.LoggerName {
+	return r.NameValue
 }
 
-type LoggerFunc func(record interface{}) error
-type RequestFunc func(request interface{}) (interface{}, error)
-
-type Logger interface {
-	PushRecord(interface{}) error
-	PullRecord(interface{}) (interface{}, error)
+type Logger struct {
+	buffer        []Record
+	flushInterval time.Duration
+	mu            sync.Mutex
+	ticker        *time.Ticker
+	running       bool
 }
 
-type BaseLogger struct {
-	logFuncs     map[LoggerName]LoggerFunc
-	requestFuncs map[LoggerName]RequestFunc
-}
-
-func NewBaseLogger() *BaseLogger {
-	return &BaseLogger{
-		logFuncs: map[LoggerName]LoggerFunc{
-			"datalogger":  data.DataLog,
-			"msglogger":   msg.MsgLog,
-			"ordlogger":   ord.OrdLog,
-			"statelogger": state.StateLog,
-		},
-		requestFuncs: map[LoggerName]RequestFunc{
-			"datalogger":  data.DataRequest,
-			"msglogger":   msg.MsgRequest,
-			"ordlogger":   ord.OrdRequest,
-			"statelogger": state.StateRequest,
-		},
+func NewLogger(flushInterval time.Duration) *Logger {
+	if flushInterval == 0 {
+		flushInterval = 5 * time.Second
 	}
+	return &Logger{
+		flushInterval: flushInterval,
+	}
+}
+
+func (l *Logger) Start() {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	if l.running {
+		return
+	}
+
+	l.running = true
+	l.ticker = time.NewTicker(l.flushInterval)
+
+	go func() {
+		for {
+			select {
+			case <-l.ticker.C:
+				l.flush()
+			}
+		}
+	}()
+}
+
+func (l *Logger) Stop() {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	if !l.running {
+		return
+	}
+
+	l.running = false
+	l.ticker.Stop()
+	l.flush()
+}
+
+func (l *Logger) PushRecord(record abstraction.LoggerRecord) error {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	if !l.running {
+		return fmt.Errorf("logger is not running")
+	}
+	if simpleRecord, ok := record.(*Record); ok {
+		l.buffer = append(l.buffer, *simpleRecord)
+	} else {
+		return fmt.Errorf("invalid record type")
+	}
+	return nil
+}
+
+func (l *Logger) PullRecord(request abstraction.LoggerRequest) (abstraction.LoggerRecord, error) {
+	panic("TODO!")
+}
+
+func (l *Logger) flush() {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	if len(l.buffer) == 0 {
+		return
+	}
+
+	filename := fmt.Sprintf("%s-%v.csv", l.buffer[0].Name(), time.Now().Format("20060102150405"))
+
+	file, err := os.Create(filename)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+
+	for _, record := range l.buffer {
+		if err := writer.Write([]string{fmt.Sprintf("%v", record.Data)}); err != nil {
+			return
+		}
+	}
+	writer.Flush()
+	l.buffer = []Record{}
 }
