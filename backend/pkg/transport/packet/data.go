d2b8336dcff0e112ce914681bc954fbe9fa59ab0
Juan Martinez <martinezalonsojn@gmail.com>
2023-11-10 17:24:15 +0100

    Document packet

--- backend/pkg/transport/packet/data.go
+++ backend/pkg/transport/packet/data.go
@@ -2,15 +2,19 @@ package packet
 
 import "github.com/HyperloopUPV-H8/h9-backend/pkg/abstraction"
 
+// DataDescriptor describes the structure of a data packet as an array of values
 type DataDescriptor []ValueDescriptor
 
+// Type assertion to check Data follows the abstraction.Packet interface
 var _ abstraction.Packet = &Data{}
 
+// Data is a data packet containing multiple values
 type Data struct {
 	id     abstraction.PacketId
 	values map[ValueName]Value
 }
 
+// NewData creates a new data packet
 func NewData(id abstraction.PacketId) *Data {
 	return &Data{
 		id:     id,
@@ -18,14 +22,17 @@ func NewData(id abstraction.PacketId) *Data {
 	}
 }
 
+// Id returns the id of the data packet
 func (data *Data) Id() abstraction.PacketId {
 	return data.id
 }
 
+// SetValue updates the value with the given name to the new value. It overwrites a value if it is already set
 func (data *Data) SetValue(name ValueName, value Value) {
 	data.values[name] = value
 }
 
+// GetValues returns all values associated with the packet
 func (data *Data) GetValues() map[ValueName]Value {
 	return data.values
 }
