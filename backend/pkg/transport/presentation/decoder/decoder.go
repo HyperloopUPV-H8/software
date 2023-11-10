c23baf8dbfe4cbfed5fc4fa819fe2d7e6fa96700
Juan Martinez <martinezalonsojn@gmail.com>
2023-11-10 16:56:06 +0100

    Add packet decoder documentation

--- backend/pkg/transport/presentation/decoder/decoder.go
+++ backend/pkg/transport/presentation/decoder/decoder.go
@@ -8,15 +8,18 @@ import (
 	"github.com/HyperloopUPV-H8/h9-backend/pkg/transport/presentation"
 )
 
+// Decoder is a common interface for packet-specific decoders
 type Decoder interface {
 	Decode(id abstraction.PacketId, reader io.Reader) (abstraction.Packet, error)
 }
 
+// Packet is the root decoder, it takes the id of the packet and decodes the rest of it
 type Packet struct {
 	idToDecoder map[abstraction.PacketId]Decoder
 	endianness  binary.ByteOrder
 }
 
+// DecodeNext reads and decodes the next packet from the input stream, returning any errors encountered
 func (decoder *Packet) DecodeNext(reader io.Reader) (abstraction.Packet, error) {
 	var id abstraction.PacketId
 	err := binary.Read(reader, decoder.endianness, &id)
