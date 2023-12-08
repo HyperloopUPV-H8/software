8394fe8156137436d7ebe0a329f06a487af201cf
Marc Sanchis <msanlli@teleco.upv.es>
2023-12-08 16:44:18 +0100

    rename subloggers map

--- backend/pkg/logger/logger.go
+++ backend/pkg/logger/logger.go
@@ -13,8 +13,8 @@ type Logger struct {
 	// An atomic boolean is used in order to use CompareAndSwap in the Start and Stop methods
 	running        *atomic.Bool
 	subloggersLock *sync.RWMutex
-	// The keys are only the loggers selected at the start of the log
-	keys map[abstraction.LoggerName]abstraction.Logger
+	// The subloggers are only the loggers selected at the start of the log
+	subloggers map[abstraction.LoggerName]abstraction.Logger
 }
 
 var _ abstraction.Logger = &Logger{}
@@ -23,7 +23,7 @@ func NewLogger(keys map[abstraction.LoggerName]abstraction.Logger) *Logger {
 	return &Logger{
 		running:        &atomic.Bool{},
 		subloggersLock: &sync.RWMutex{},
-		keys:           keys,
+		subloggers:     keys,
 	}
 }
 
@@ -33,7 +33,7 @@ func (logger *Logger) Start() error {
 		return nil
 	}
 
-	for _, key := range logger.keys {
+	for _, key := range logger.subloggers {
 		go key.Start()
 	}
 
@@ -46,7 +46,7 @@ func (logger *Logger) Start() error {
 
 // PushRecord works as a proxy for the PushRecord method of the subloggers
 func (logger *Logger) PushRecord(record abstraction.LoggerRecord) error {
-	loggerChecked, ok := logger.keys[record.Name()]
+	loggerChecked, ok := logger.subloggers[record.Name()]
 	if !ok {
 		return ErrLoggerNotFound{record.Name()}
 	}
@@ -55,7 +55,7 @@ func (logger *Logger) PushRecord(record abstraction.LoggerRecord) error {
 
 // PullRecord works as a proxy for the PullRecord method of the subloggers
 func (logger *Logger) PullRecord(request abstraction.LoggerRequest) (abstraction.LoggerRecord, error) {
-	loggerChecked, ok := logger.keys[request.Name()]
+	loggerChecked, ok := logger.subloggers[request.Name()]
 	if !ok {
 		return nil, ErrLoggerNotFound{request.Name()}
 	}
@@ -74,13 +74,13 @@ func (logger *Logger) Stop() error {
 	// The waitgroup is used in order to wait for all the subloggers to stop
 	// before closing the main logger
 	var wg sync.WaitGroup
-	for name := range logger.keys {
+	for name := range logger.subloggers {
 		wg.Add(1)
 
 		go func(sublogger abstraction.Logger) {
 			defer wg.Done()
 			sublogger.Stop()
-		}(logger.keys[name])
+		}(logger.subloggers[name])
 	}
 	wg.Wait()
 
