5d51e11e61e67467900577687e89105b7d400f3f
jmaralo <martinezalonsojn@gmail.com>
2023-12-23 21:22:04 +0100

    Remove websocket client implementation from broker

--- backend/pkg/broker/broker.go
+++ backend/pkg/broker/broker.go
@@ -3,15 +3,13 @@ package broker
 import (
 	"github.com/HyperloopUPV-H8/h9-backend/pkg/abstraction"
 	"github.com/HyperloopUPV-H8/h9-backend/pkg/broker/topics"
-	"github.com/HyperloopUPV-H8/h9-backend/pkg/broker/websocket"
 )
 
 var _ abstraction.Broker = &Broker{}
 
 type Broker struct {
-	topics  map[abstraction.BrokerTopic]topics.Handler
-	clients *websocket.Pool
-	api     abstraction.BrokerAPI
+	topics map[abstraction.BrokerTopic]topics.Handler
+	api    abstraction.BrokerAPI
 }
 
 func (broker *Broker) Push(push abstraction.BrokerPush) error {
@@ -40,16 +38,8 @@ func (broker *Broker) SetAPI(api abstraction.BrokerAPI) {
 	broker.api = api
 }
 
-func (broker *Broker) SetPool(pool *websocket.Pool) {
-	broker.clients = pool
-	for _, topic := range broker.topics {
-		topic.SetPool(pool)
-	}
-}
-
 func (broker *Broker) AddTopic(topic abstraction.BrokerTopic, handler topics.Handler) {
 	handler.SetAPI(broker)
-	handler.SetPool(broker.clients)
 	broker.topics[topic] = handler
 }
 
