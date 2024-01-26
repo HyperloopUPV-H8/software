f6f3866d794da2f2db53745dd0cb606578a24f3e
Marc Sanchis <msanlli@teleco.upv.es>
2024-01-26 17:52:32 +0100

    blcu request func

--- backend/pkg/blcu/blcu.go
+++ backend/pkg/blcu/blcu.go
@@ -2,6 +2,7 @@ package blcu
 
 import (
 	"github.com/HyperloopUPV-H8/h9-backend/pkg/abstraction"
+	broker "github.com/HyperloopUPV-H8/h9-backend/pkg/broker/topics/blcu"
 )
 
 type BLCU struct {
@@ -11,5 +12,16 @@ type BLCU struct {
 func NewBLCU(boarMap map[string]abstraction.BoardId) *BLCU {
 	return &BLCU{
 		boardToId: boarMap,
+	}
+}
+
+func (blcu *BLCU) Request(request abstraction.BrokerRequest) (abstraction.BrokerResponse, error) {
+	switch request.Topic() {
+	case "blcu/download":
+		return broker.Download.Pull(request)
+	case "blcu/upload":
+		return broker.Upload.Pull(request)
+	default:
+		return nil, ErrTopicNotFound{}
 	}
 }
