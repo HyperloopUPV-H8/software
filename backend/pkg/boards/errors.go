92281ea052a08fbe8c13718f61521a5047201e71
msanlli <markix95@gmail.com>
2024-03-07 19:21:00 +0100

    fix boards error printing

--- backend/pkg/boards/errors.go
+++ backend/pkg/boards/errors.go
@@ -12,8 +12,8 @@ type ErrNewClientFailed struct {
 	Inner     error
 }
 
-func (err ErrNewClientFailed) Error() {
-	fmt.Sprintf("Error creating new client at %s", err.Timestamp.Format(time.RFC3339))
+func (err ErrNewClientFailed) Error() string {
+	return fmt.Sprintf("error creating new client at %s", err.Timestamp.Format(time.RFC3339))
 }
 
 type ErrInvalidBoardEvent struct {
@@ -21,8 +21,8 @@ type ErrInvalidBoardEvent struct {
 	Timestamp time.Time
 }
 
-func (err ErrInvalidBoardEvent) Error() {
-	fmt.Sprintf("Invalid board event %s at %s", err.Event, err.Timestamp.Format(time.RFC3339))
+func (err ErrInvalidBoardEvent) Error() string {
+	return fmt.Sprintf("error invalid board event %s at %s", err.Event, err.Timestamp.Format(time.RFC3339))
 }
 
 type ErrReadingFileFailed struct {
@@ -31,8 +31,8 @@ type ErrReadingFileFailed struct {
 	Inner     error
 }
 
-func (err ErrReadingFileFailed) Error() {
-	fmt.Sprintf("Invalid board id %s at %s", err.Filename, err.Timestamp.Format(time.RFC3339))
+func (err ErrReadingFileFailed) Error() string {
+	return fmt.Sprintf("error invalid board id %s at %s", err.Filename, err.Timestamp.Format(time.RFC3339))
 }
 
 type ErrSendMessageFailed struct {
@@ -40,16 +40,16 @@ type ErrSendMessageFailed struct {
 	Inner     error
 }
 
-func (err ErrSendMessageFailed) Error() {
-	fmt.Sprintf("Error sending message at %s", err.Timestamp.Format(time.RFC3339))
+func (err ErrSendMessageFailed) Error() string {
+	return fmt.Sprintf("error sending message at %s", err.Timestamp.Format(time.RFC3339))
 }
 
 type ErrNotAllBytesWritten struct {
 	Timestamp time.Time
 }
 
-func (err ErrNotAllBytesWritten) Error() {
-	fmt.Sprintf("Not all bytes written at %s", err.Timestamp.Format(time.RFC3339))
+func (err ErrNotAllBytesWritten) Error() string {
+	return fmt.Sprintf("error not all bytes written at %s", err.Timestamp.Format(time.RFC3339))
 }
 
 type ErrDownloadFailure struct {
@@ -57,8 +57,8 @@ type ErrDownloadFailure struct {
 	Inner     error
 }
 
-func (err ErrDownloadFailure) Error() {
-	fmt.Sprintf("Download failure at %s", err.Timestamp.Format(time.RFC3339))
+func (err ErrDownloadFailure) Error() string {
+	return fmt.Sprintf("error download failure at %s", err.Timestamp.Format(time.RFC3339))
 }
 
 type ErrUploadFailure struct {
@@ -66,6 +66,6 @@ type ErrUploadFailure struct {
 	Inner     error
 }
 
-func (err ErrUploadFailure) Error() {
-	fmt.Sprintf("Upload failure at %s", err.Timestamp.Format(time.RFC3339))
+func (err ErrUploadFailure) Error() string {
+	return fmt.Sprintf("error upload failure at %s", err.Timestamp.Format(time.RFC3339))
 }
