331ad3f91fb6504db18051ba606b17d51604cf43
jmaralo <martinezalonsojn@gmail.com>
2024-04-15 13:34:57 +0200

    Add checkbox to switch between displaying the average value and the latest value

--- common-front/lib/store/measurementsStore.ts
+++ common-front/lib/store/measurementsStore.ts
@@ -1,4 +1,8 @@
-import { Measurement, NumericMeasurement, isNumericMeasurement } from "../models";
+import {
+    Measurement,
+    NumericMeasurement,
+    isNumericMeasurement,
+} from '../models';
 import {
     getBooleanMeasurement,
     getEnumMeasurement,
@@ -6,30 +10,32 @@ import {
     isNumericAdapter,
     PodDataAdapter,
     PacketUpdate,
-} from "../adapters";
-import { create } from "zustand";
-import { isNumericType } from "../BackendTypes";
+} from '../adapters';
+import { create } from 'zustand';
+import { isNumericType } from '../BackendTypes';
+import { NumericValue } from '../models/PodData/Measurement';
 
-export type Measurements = Record<string, Measurement>
+export type Measurements = Record<string, Measurement>;
 export type MeasurementId = string;
 export type MeasurementName = string;
 export type MeasurementColor = string;
 export type MeasurementUnits = string;
 export type UpdateFunction = () => number;
 export type NumericMeasurementInfo = {
-  readonly id: MeasurementId;
-  readonly name: MeasurementName;
-  readonly range: [number | null, number | null];
-  readonly color: MeasurementColor;
-  readonly units: MeasurementUnits;
-  readonly getUpdate: UpdateFunction;
+    readonly id: MeasurementId;
+    readonly name: MeasurementName;
+    readonly range: [number | null, number | null];
+    readonly color: MeasurementColor;
+    readonly units: MeasurementUnits;
+    readonly getUpdate: UpdateFunction;
 };
 
 export interface MeasurementsStore {
     measurements: Measurements;
     packetIdToBoard: Record<number, string>;
     initMeasurements: (podDataAdapter: PodDataAdapter) => void;
-    updateMeasurements: (measurements: Record<string, PacketUpdate>) => void
+    updateMeasurements: (measurements: Record<string, PacketUpdate>) => void;
+    showMeasurementLatest: (id: string, showLatest: boolean) => void;
     getMeasurement: (id: MeasurementId) => Measurement;
     getNumericMeasurementInfo: (id: MeasurementId) => NumericMeasurementInfo;
     getMeasurementFallback: (id: MeasurementId) => Measurement;
@@ -43,43 +49,53 @@ export const useMeasurementsStore = create<MeasurementsStore>((set, get) => ({
     /**
      * Reducer that receives a PodDataAdapter and initializes the measurements
      * and packetIdToBoard map in state.
-     * @param {PodDataAdapter} podDataAdapter 
+     * @param {PodDataAdapter} podDataAdapter
      * @returns {Measurements}
      */
     initMeasurements: (podDataAdapter: PodDataAdapter) => {
-        set(state => ({
+        set((state) => ({
             ...state,
             measurements: createMeasurementsFromPodDataAdapter(podDataAdapter),
             packetIdToBoard: getPacketIdToBoard(podDataAdapter),
-        }))
+        }));
     },
 
     /**
      * Reducer that updates the measurements in the state.
      * It receives a measurements map with PacketUpdates, extract the measurements
      * from each of them and updates the measurements.
-     * @param {Record<string, PacketUpdate>} measurements 
+     * @param {Record<string, PacketUpdate>} measurements
      */
     updateMeasurements: (measurements: Record<string, PacketUpdate>) => {
-
         const measurementsDraft = get().measurements;
 
-        for(const update of Object.values(measurements)) {
-            for (const [id, mUpdate] of Object.entries(update.measurementUpdates)) {
+        for (const update of Object.values(measurements)) {
+            for (const [id, mUpdate] of Object.entries(
+                update.measurementUpdates
+            )) {
                 const boardName = get().packetIdToBoard[update.id];
                 if (!boardName) {
                     continue;
                 }
 
                 const measurementId = `${boardName}/${id}`;
-                measurementsDraft[measurementId].value = mUpdate;
+                if (typeof mUpdate != 'string' && typeof mUpdate != 'boolean') {
+                    (
+                        measurementsDraft[measurementId].value as NumericValue
+                    ).last = mUpdate.last;
+                    (
+                        measurementsDraft[measurementId].value as NumericValue
+                    ).average = mUpdate.average;
+                } else {
+                    measurementsDraft[measurementId].value = mUpdate;
+                }
             }
         }
-        
-        set(state => ({
+
+        set((state) => ({
             ...state,
-            measurements: measurementsDraft
-        }))
+            measurements: measurementsDraft,
+        }));
     },
 
     clearMeasurements: (board: string) => {
@@ -91,19 +107,39 @@ export const useMeasurementsStore = create<MeasurementsStore>((set, get) => ({
                     measurementsDraft[measurementId].value = {
                         average: 0,
                         last: 0,
-                    }
-                } else if (measurementsDraft[measurementId].type == "bool") {
-                    measurementsDraft[measurementId].value = false
+                        showLatest: false,
+                    };
+                } else if (measurementsDraft[measurementId].type == 'bool') {
+                    measurementsDraft[measurementId].value = false;
                 } else {
-                    measurementsDraft[measurementId].value = "Default"
+                    measurementsDraft[measurementId].value = 'Default';
                 }
             }
         }
 
-        set(state => ({
+        set((state) => ({
             ...state,
-            measurements: measurementsDraft
-        }))
+            measurements: measurementsDraft,
+        }));
+    },
+
+    showMeasurementLatest: (id: string, showLatest: boolean) => {
+        set((state) => {
+            const draft = state.measurements;
+
+            if (
+                id in draft &&
+                typeof draft[id].value != 'string' &&
+                typeof draft[id].value != 'boolean'
+            ) {
+                (draft[id].value as NumericValue).showLatest = showLatest;
+            }
+
+            return {
+                ...state,
+                measurements: draft,
+            };
+        });
     },
 
     getMeasurement: (id: string) => {
@@ -127,17 +163,19 @@ export const useMeasurementsStore = create<MeasurementsStore>((set, get) => ({
     },
 
     getMeasurementFallback: (id: string) => {
-        return get().measurements[id] ?? {
-            id: "Default",
-            name: "Default",
-            safeRange: [null, null],
-            warningRange: [null, null],
-            type: "uint8",
-            units: "A",
-            value: { average: 0, last: 0 },
-        };
-    }
-}))
+        return (
+            get().measurements[id] ?? {
+                id: 'Default',
+                name: 'Default',
+                safeRange: [null, null],
+                warningRange: [null, null],
+                type: 'uint8',
+                units: 'A',
+                value: { average: 0, last: 0 },
+            }
+        );
+    },
+}));
 
 function createMeasurementsFromPodDataAdapter(
     podDataAdapter: PodDataAdapter
@@ -150,7 +188,7 @@ function createMeasurementsFromPodDataAdapter(
                 const id = `${board.name}/${adapter.id}`;
                 if (isNumericAdapter(adapter)) {
                     measurements[id] = getNumericMeasurement(id, adapter);
-                } else if (adapter.type == "bool") {
+                } else if (adapter.type == 'bool') {
                     measurements[id] = getBooleanMeasurement(id, adapter);
                 } else {
                     measurements[id] = getEnumMeasurement(id, adapter);
--- ethernet-view/src/components/ReceiveTable/BoardView/PacketView/MeasurementView/MeasurementView.tsx
+++ ethernet-view/src/components/ReceiveTable/BoardView/PacketView/MeasurementView/MeasurementView.tsx
@@ -1,40 +1,56 @@
-import styles from "./MeasurementView.module.scss";
-import { Measurement, isNumericMeasurement } from "common";
-import { useUpdater } from "./useUpdater";
+import styles from './MeasurementView.module.scss';
+import {
+    Measurement,
+    isNumericMeasurement,
+    useMeasurementsStore,
+} from 'common';
+import { useUpdater } from './useUpdater';
+import { FormEvent } from 'react';
 
 type Props = {
     measurement: Measurement;
 };
 
 export const MeasurementView = ({ measurement }: Props) => {
+    const setShowMeasurementLatest = useMeasurementsStore(
+        (state) => (showLatest: boolean) =>
+            state.showMeasurementLatest(measurement.id, showLatest)
+    );
     const isNumeric = isNumericMeasurement(measurement);
 
     const { valueRef } = useUpdater(
         measurement.id,
         isNumeric
-            ? measurement.value.average.toFixed(3)
+            ? measurement.value.showLatest
+                ? measurement.value.last.toFixed(3)
+                : measurement.value.average.toFixed(3)
             : measurement.value.toString()
     );
+
+    const onLatestValueChange = (event: FormEvent<HTMLInputElement>) => {
+        setShowMeasurementLatest(event.currentTarget.checked);
+    };
 
     return (
         <>
             <span className={styles.name}>{measurement.name}</span>
             {isNumeric && (
                 <>
-                    <span
-                        ref={valueRef}
-                        className={styles.value}
-                    ></span>
+                    <input
+                        type="checkbox"
+                        defaultChecked={false}
+                        className={styles.show_last}
+                        title="Show latest value"
+                        onInput={onLatestValueChange}
+                    />
+                    <span ref={valueRef} className={styles.value}></span>
                     <span className={styles.units}>{measurement.units}</span>
                     <span className={styles.type}>{measurement.type}</span>
                 </>
             )}
             {!isNumeric && (
                 <>
-                    <span
-                        ref={valueRef}
-                        className={styles.value}
-                    ></span>
+                    <span ref={valueRef} className={styles.value}></span>
                     <span className={styles.type}>{measurement.type}</span>
                 </>
             )}
