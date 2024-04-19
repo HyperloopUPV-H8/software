c0db4b3b01ed9f144210732bcabbd94f5c17dc88
msanlli <markix95@gmail.com>
2024-04-19 19:50:02 +0200

    transport test

--- backend/pkg/transport/transport_test.go
+++ backend/pkg/transport/transport_test.go
@@ -1,7 +1,7 @@
 package transport_test
 
 import (
-	"github.com/HyperloopUPV-H8/h9-backend/pkg/transport"
+	transport_module "github.com/HyperloopUPV-H8/h9-backend/pkg/transport"
 	"github.com/HyperloopUPV-H8/h9-backend/pkg/transport/network/tcp"
 	"github.com/rs/zerolog"
 	"net"
@@ -12,23 +12,23 @@ import (
 func TestTransport(t *testing.T) {
 	// Create server
 	logger := zerolog.New(os.Stdout)
-	err := transport.NewTransport(logger).HandleServer(tcp.NewServerConfig(), "127.0.0.1:8080")
+	transport := transport_module.NewTransport(logger)
+	err := transport.HandleServer(tcp.NewServerConfig(), "127.0.0.1:8080")
 	if err != nil {
 		t.Errorf("Error creating server at 127.0.0.1:8080: %s", err)
 	}
 
 	// Create client
 	addr, _ := net.ResolveTCPAddr("tcp", "127.0.0.1:3000")
-	err = transport.NewTransport(logger).HandleClient(tcp.NewClientConfig(addr), "127.0.0.1:8080")
+	err = transport.HandleClient(tcp.NewClientConfig(addr), "127.0.0.1:8080")
 	if err != nil {
 		t.Errorf("Error creating client at 127.0.0.1:3000: %s", err)
 	}
 
 	// Create client with wrong address
 	addr, _ = net.ResolveTCPAddr("tcp", "127.0.0.1:3030")
-	err = transport.NewTransport(logger).HandleClient(tcp.NewClientConfig(addr), "127.0.0.1:8000")
+	err = transport.HandleClient(tcp.NewClientConfig(addr), "127.0.0.1:8000")
 	if err == nil {
 		t.Errorf("Expected error creating client at wrong address, got nil")
 	}
-
 }
