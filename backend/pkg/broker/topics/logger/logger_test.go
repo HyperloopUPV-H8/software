91318d27212ef61e77e34fa0c2ca41c49c64a967
msanlli <markix95@gmail.com>
2024-05-05 16:12:41 +0200

    logger cm  test

--- backend/pkg/broker/topics/logger/logger_test.go
+++ backend/pkg/broker/topics/logger/logger_test.go
@@ -1,6 +1,7 @@
 package data_test
 
 import (
+	"encoding/json"
 	"github.com/HyperloopUPV-H8/h9-backend/pkg/broker"
 	data "github.com/HyperloopUPV-H8/h9-backend/pkg/broker/topics/logger"
 	"github.com/HyperloopUPV-H8/h9-backend/pkg/websocket"
@@ -41,4 +42,32 @@ func TestLoggerEnable(t *testing.T) {
 	err = log.Push(data.NewStatus(true))
 
 	assert.NoError(t, err)
+}
+
+func TestClientMessage(t *testing.T) {
+	logger := zerolog.New(os.Stdout)
+	u := url.URL{Scheme: "ws", Host: "localhost:8080", Path: "/blcucm"}
+
+	clientChan := make(chan *websocket.Client)
+	http.HandleFunc("/blcucm", func(writer http.ResponseWriter, request *http.Request) {
+		upgrader := websocket.NewUpgrader(clientChan, logger)
+		upgrader.Upgrade(writer, request, nil)
+	})
+	go http.ListenAndServe(":8080", nil)
+
+	c, _, err := ws.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		t.Fatal("Error dialing:", err)
+	}
+
+	api := broker.New(logger)
+	pool := websocket.NewPool(clientChan, logger)
+
+	websocket.NewClient(c)
+
+	log := data.NewEnableTopic()
+	log.SetPool(pool)
+	log.SetAPI(api)
+
+	log.ClientMessage(websocket.ClientId{}, &websocket.Message{"test", json.RawMessage("test")})
 }
