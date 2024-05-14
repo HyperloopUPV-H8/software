9b25becab9ec14d84125ba1d8b5f5ad8eeeea4dc
msanlli <markix95@gmail.com>
2024-05-14 18:29:19 +0200

    order logger test

--- backend/pkg/logger/logger_test.go
+++ backend/pkg/logger/logger_test.go
@@ -6,7 +6,8 @@ import (
 	"github.com/HyperloopUPV-H8/h9-backend/pkg/abstraction"
 	"github.com/HyperloopUPV-H8/h9-backend/pkg/logger"
 	"github.com/HyperloopUPV-H8/h9-backend/pkg/logger/data"
-	packetData "github.com/HyperloopUPV-H8/h9-backend/pkg/transport/packet/data"
+	"github.com/HyperloopUPV-H8/h9-backend/pkg/logger/order"
+	dataPacketer "github.com/HyperloopUPV-H8/h9-backend/pkg/transport/packet/data"
 	"github.com/rs/zerolog"
 	"os"
 	"path"
@@ -15,31 +16,35 @@ import (
 )
 
 func TestLogger_Data(t *testing.T) {
-	sublogger := data.NewLogger()
-	logger := logger.NewLogger(map[abstraction.LoggerName]abstraction.Logger{
-		data.Name: sublogger,
+	dataSublogger := data.NewLogger()
+	orderSublogger := order.NewLogger()
+	loggerHandler := logger.NewLogger(map[abstraction.LoggerName]abstraction.Logger{
+		data.Name:  dataSublogger,
+		order.Name: orderSublogger,
 	}, zerolog.New(os.Stdout).With().Timestamp().Logger())
 
-	if err := logger.Start(); err != nil {
+	if err := loggerHandler.Start(); err != nil {
 		t.Error(err)
 	}
 
 	timestamp := time.Now()
-	packet := packetData.NewPacketWithValues(
+
+	// Data
+	dataPacket := dataPacketer.NewPacketWithValues(
 		0,
-		map[packetData.ValueName]packetData.Value{
-			"test": packetData.NewBooleanValue(true),
+		map[dataPacketer.ValueName]dataPacketer.Value{
+			"test": dataPacketer.NewBooleanValue(true),
 		},
-		map[packetData.ValueName]bool{
+		map[dataPacketer.ValueName]bool{
 			"test": true,
 		})
-	record := &data.Record{
-		Packet:    packet,
+	dataRecord := &data.Record{
+		Packet:    dataPacket,
 		From:      "test",
 		To:        "test",
 		Timestamp: timestamp,
 	}
-	err := logger.PushRecord(record)
+	err := loggerHandler.PushRecord(dataRecord)
 	if err != nil {
 		t.Error(err)
 	}
@@ -60,11 +65,44 @@ func TestLogger_Data(t *testing.T) {
 		t.Error(err)
 	}
 
-	if output[2] != "test" || output[3] != "true" {
+	if output[0] != fmt.Sprint(timestamp.UnixMilli()) || output[1] != "test" || output[2] != "test" || output[3] != "true" {
 		t.Errorf("expected [test true], got %v", output)
 	}
 
-	if closeErr := logger.Stop(); closeErr != nil {
+	// Order
+	orderPacket := dataPacketer.NewPacket(0)
+	orderRecord := &order.Record{
+		Packet:    orderPacket,
+		From:      "test",
+		To:        "test",
+		Timestamp: timestamp,
+	}
+	err = loggerHandler.PushRecord(orderRecord)
+	if err != nil {
+		t.Error(err)
+	}
+
+	filename = path.Join(
+		"logger/order",
+		fmt.Sprintf("order_%s", timestamp.Format(time.RFC3339)),
+		"order.csv",
+	)
+	file, err = os.Open(filename)
+	if err != nil {
+		t.Error(err)
+	}
+	defer file.Close()
+
+	output, err = csv.NewReader(file).Read()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if output[0] != fmt.Sprint(timestamp.UnixMilli()) || output[1] != "test" || output[2] != "test" {
+		t.Errorf("expected [test test], got %v", output)
+	}
+
+	if closeErr := loggerHandler.Stop(); closeErr != nil {
 		t.Error(err)
 	}
 
