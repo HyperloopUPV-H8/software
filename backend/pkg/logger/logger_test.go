9f90bff737a2dd249747b8fbbc8b6183a4ecc2fc
msanlli <markix95@gmail.com>
2024-05-15 13:30:02 +0200

    protection logger fix

--- backend/pkg/logger/logger_test.go
+++ backend/pkg/logger/logger_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/HyperloopUPV-H8/h9-backend/pkg/logger/data"
 	"github.com/HyperloopUPV-H8/h9-backend/pkg/logger/order"
 	"github.com/HyperloopUPV-H8/h9-backend/pkg/logger/protection"
+	"github.com/HyperloopUPV-H8/h9-backend/pkg/logger/state"
 	dataPacketer "github.com/HyperloopUPV-H8/h9-backend/pkg/transport/packet/data"
 	protectionPacketer "github.com/HyperloopUPV-H8/h9-backend/pkg/transport/packet/protection"
 	"github.com/rs/zerolog"
@@ -23,17 +24,21 @@ func TestLogger(t *testing.T) {
 	protectionSublogger := protection.NewLogger(map[abstraction.BoardId]string{
 		0: "test",
 	})
+	stateSublogger := state.NewLogger()
 	loggerHandler := logger.NewLogger(map[abstraction.LoggerName]abstraction.Logger{
 		data.Name:       dataSublogger,
 		order.Name:      orderSublogger,
 		protection.Name: protectionSublogger,
+		state.Name:      stateSublogger,
 	}, zerolog.New(os.Stdout).With().Timestamp().Logger())
 
 	if err := loggerHandler.Start(); err != nil {
 		t.Error(err)
 	}
 
-	timestamp := time.Now()
+	timestamp := logger.Timestamp
+
+	time.Sleep(100 * time.Millisecond)
 
 	// Data
 	dataPacket := dataPacketer.NewPacketWithValues(
@@ -44,6 +49,7 @@ func TestLogger(t *testing.T) {
 		map[dataPacketer.ValueName]bool{
 			"test": true,
 		})
+	dataPacketTime := time.Now()
 	dataRecord := &data.Record{
 		Packet:    dataPacket,
 		From:      "test",
@@ -57,7 +63,7 @@ func TestLogger(t *testing.T) {
 
 	filename := path.Join(
 		"logger/data",
-		fmt.Sprintf("data_%s", timestamp.Format("2006_01_02_15_04_05")),
+		fmt.Sprintf("%s", timestamp.Format(logger.TimestampFormat)),
 		fmt.Sprintf("%s.csv", "test"),
 	)
 	file, err := os.Open(filename)
@@ -71,12 +77,13 @@ func TestLogger(t *testing.T) {
 		t.Error(err)
 	}
 
-	if output[0] != fmt.Sprint(timestamp.UnixMilli()) || output[1] != "test" || output[2] != "test" || output[3] != "true" {
-		t.Errorf("dataErr: expected [test true], got %v", output)
+	if output[0] != fmt.Sprint(dataPacketTime.UnixMilli()) || output[1] != "test" || output[2] != "test" || output[3] != "true" {
+		t.Errorf("dataErr: expected [%v test test true], got %v", timestamp.UnixMilli(), output)
 	}
 
 	// Order
 	orderPacket := dataPacketer.NewPacket(0)
+	orderPacketTime := time.Now()
 	orderRecord := &order.Record{
 		Packet:    orderPacket,
 		From:      "test",
@@ -90,7 +97,7 @@ func TestLogger(t *testing.T) {
 
 	filename = path.Join(
 		"logger/order",
-		fmt.Sprintf("order_%s", timestamp.Format("2006_01_02_15_04_05")),
+		fmt.Sprintf("%s", timestamp.Format(logger.TimestampFormat)),
 		"order.csv",
 	)
 	file, err = os.Open(filename)
@@ -104,12 +111,28 @@ func TestLogger(t *testing.T) {
 		t.Error(err)
 	}
 
-	if output[0] != fmt.Sprint(timestamp.UnixMilli()) || output[1] != "test" || output[2] != "test" {
+	if output[0] != fmt.Sprint(orderPacketTime.UnixMilli()) || output[1] != "test" || output[2] != "test" {
 		t.Errorf("orderErr: expected [test test], got %v", output)
 	}
 
 	// Protection
 	protectionPacket := protectionPacketer.NewPacket(0, protectionPacketer.Ok)
+	protectionPacket.Timestamp = &protectionPacketer.Timestamp{
+		Counter: 0,
+		Second:  0,
+		Minute:  0,
+		Hour:    0,
+		Day:     0,
+		Month:   0,
+		Year:    0,
+	}
+	protectionPacket.Name = "test"
+	protectionPacket.Kind = protectionPacketer.EqualsKind
+	protectionPacket.Data = &protectionPacketer.Equals[int8]{
+		Target: 0,
+		Value:  0,
+	}
+	protectionPacket.Type = protectionPacketer.Uint8Type
 	protectionRecord := &protection.Record{
 		Packet:    protectionPacket,
 		BoardId:   abstraction.BoardId(0),
@@ -117,6 +140,8 @@ func TestLogger(t *testing.T) {
 		To:        "test",
 		Timestamp: timestamp,
 	}
+	protectionPacketTime := protectionPacket.Timestamp.ToTime()
+
 	err = loggerHandler.PushRecord(protectionRecord)
 	if err != nil {
 		t.Error(err)
@@ -124,7 +149,7 @@ func TestLogger(t *testing.T) {
 
 	filename = path.Join(
 		"logger/protections",
-		fmt.Sprintf("protections_%s", timestamp.Format("2006_01_02_15_04_05")),
+		fmt.Sprintf("%s", timestamp.Format(logger.TimestampFormat)),
 		fmt.Sprintf("%s.csv", "test"),
 	)
 	file, err = os.Open(filename)
@@ -138,7 +163,7 @@ func TestLogger(t *testing.T) {
 		t.Error(err)
 	}
 
-	if output[0] != fmt.Sprint(timestamp.UnixMilli()) || output[1] != "test" || output[2] != "test" || output[3] != "0" {
+	if output[0] != fmt.Sprint(timestamp.UnixMilli()) || output[1] != "test" || output[2] != "test" || output[3] != "0" || output[4] != "7" || output[5] != "3" || output[6] != "test" || output[7] != "&{0 0}" || output[8] != protectionPacketTime.Format(time.RFC3339) {
 		t.Errorf("orderErr: expected [test test 0], got %v", output)
 	}
 
@@ -146,5 +171,5 @@ func TestLogger(t *testing.T) {
 		t.Error(err)
 	}
 
-	// os.RemoveAll("logger")
+	os.RemoveAll("logger")
 }
