fdd40e4cd817487dbc7477d2edde7b39ff8c311c
jmaralo <martinezalonsojn@gmail.com>
2024-05-15 17:49:16 +0200

    Make charts show 3 decimals

--- ethernet-view/src/components/ChartMenu/ChartElement/ChartCanvas/ChartCanvas.tsx
+++ ethernet-view/src/components/ChartMenu/ChartElement/ChartCanvas/ChartCanvas.tsx
@@ -1,8 +1,22 @@
-import { MeasurementId, NumericMeasurementInfo, UpdateFunction, useGlobalTicker } from "common";
-import { ColorType, IChartApi, ISeriesApi, UTCTimestamp, createChart } from "lightweight-charts";
-import { useEffect, useRef } from "react";
+import {
+    MeasurementId,
+    NumericMeasurementInfo,
+    UpdateFunction,
+    useGlobalTicker,
+} from 'common';
+import {
+    ColorType,
+    IChartApi,
+    ISeriesApi,
+    UTCTimestamp,
+    createChart,
+} from 'lightweight-charts';
+import { useEffect, useRef } from 'react';
 
-type DataSerieAndUpdater = Map<MeasurementId, [ISeriesApi<"Line">, UpdateFunction]>;
+type DataSerieAndUpdater = Map<
+    MeasurementId,
+    [ISeriesApi<'Line'>, UpdateFunction]
+>;
 
 interface Props {
     measurementsInChart: NumericMeasurementInfo[];
@@ -11,7 +25,6 @@ interface Props {
 const CHART_HEIGHT = 300;
 
 export const ChartCanvas = ({ measurementsInChart }: Props) => {
-
     const chart = useRef<IChartApi | null>(null);
     const chartContainerRef = useRef<HTMLDivElement>(null);
     const chartDataSeries = useRef<DataSerieAndUpdater>(new Map());
@@ -19,57 +32,68 @@ export const ChartCanvas = ({ measurementsInChart }: Props) => {
     useEffect(() => {
         const handleResize = () => {
             if (chartContainerRef.current)
-            if(chart)
-            chart.current?.applyOptions({ width: chartContainerRef.current.clientWidth });
+                if (chart)
+                    chart.current?.applyOptions({
+                        width: chartContainerRef.current.clientWidth,
+                    });
         };
 
         const resizeObserver = new ResizeObserver(handleResize);
         if (chartContainerRef.current)
-        resizeObserver.observe(chartContainerRef.current);
+            resizeObserver.observe(chartContainerRef.current);
 
         if (chartContainerRef.current) {
-            if(chart)
-            chart.current = createChart(chartContainerRef.current, {
-                layout: {
-                    background: { type: ColorType.Solid, color: "white" },
-                    textColor: "black",
-                },
-                width: chartContainerRef.current.clientWidth,
-                height: CHART_HEIGHT,
-                timeScale: {
-                    timeVisible: true,
-                    secondsVisible: true,
-                    fixLeftEdge: true,
-                    fixRightEdge: true,
-                    lockVisibleTimeRangeOnResize: true,
-                    rightBarStaysOnScroll: true,
-                }
-            });
+            if (chart) {
+                chart.current = createChart(chartContainerRef.current, {
+                    layout: {
+                        background: { type: ColorType.Solid, color: 'white' },
+                        textColor: 'black',
+                    },
+                    width: chartContainerRef.current.clientWidth,
+                    height: CHART_HEIGHT,
+                    timeScale: {
+                        timeVisible: true,
+                        secondsVisible: true,
+                        fixLeftEdge: true,
+                        fixRightEdge: true,
+                        lockVisibleTimeRangeOnResize: true,
+                        rightBarStaysOnScroll: true,
+                    },
+                });
+            }
         }
 
         return () => {
             resizeObserver.disconnect();
             chart.current?.remove();
-        }
+        };
     }, []);
 
     useEffect(() => {
         chartDataSeries.current.clear();
         measurementsInChart.forEach((measurement) => {
-            if(chart.current)
-            chartDataSeries.current.set(measurement.id, [chart.current.addLineSeries({color: measurement.color}), measurement.getUpdate]);
-        });
-    })
-
-    useGlobalTicker(() => {
-        const now = Date.now() / 1000 as UTCTimestamp;
-        chartDataSeries.current?.forEach((serieAndUpdater) => {
-            const [DataSerie, Updater] = serieAndUpdater;
-            DataSerie.update({ time: now, value: Updater() })
+            if (chart.current)
+                chartDataSeries.current.set(measurement.id, [
+                    chart.current.addLineSeries({
+                        color: measurement.color,
+                        priceFormat: {
+                            type: 'price',
+                            precision: 3,
+                            minMove: 0.001,
+                        },
+                    }),
+                    measurement.getUpdate,
+                ]);
         });
     });
 
-    return (
-        <div ref={chartContainerRef}></div>
-    );
+    useGlobalTicker(() => {
+        const now = (Date.now() / 1000) as UTCTimestamp;
+        chartDataSeries.current?.forEach((serieAndUpdater) => {
+            const [DataSerie, Updater] = serieAndUpdater;
+            DataSerie.update({ time: now, value: Updater() });
+        });
+    });
+
+    return <div ref={chartContainerRef}></div>;
 };
