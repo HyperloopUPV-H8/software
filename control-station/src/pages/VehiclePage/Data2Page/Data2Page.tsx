cbf409574212f4a79f3704468a2b98c33f5c1c5a
jmaralo <martinezalonsojn@gmail.com>
2024-06-26 20:59:30 +0200

    update Data2Page CSS

--- control-station/src/pages/VehiclePage/Data2Page/Data2Page.tsx
+++ control-station/src/pages/VehiclePage/Data2Page/Data2Page.tsx
@@ -1,11 +1,13 @@
-import styles from './BoardsPage.module.scss';
+import styles from './Data2Page.module.scss';
 import { LCU } from '../Boards/LCU/LCU';
 import { PCU } from '../Boards/PCU/PCU';
 
 export const Data2Page = () => {
     return (
-        <div className={styles.boardsPage}>
-            <LCU />
+        <div className={styles.data2_page}>
+            <div className={styles.column}>
+                <LCU />
+            </div>
 
             <div className={styles.column}>
                 <PCU />
