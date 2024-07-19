7220bfbb78257002098ed9d8c7dacfb55ea1948e
jmaralo <martinezalonsojn@gmail.com>
2024-07-19 10:38:28 +0200

    This should be the last fixed orders change

--- control-station/src/pages/VehiclePage/Data2Page/FixedOrders.tsx
+++ control-station/src/pages/VehiclePage/Data2Page/FixedOrders.tsx
@@ -54,8 +54,7 @@ export const emergencyStopOrders: Order[] = [
 ];
 
 export const desiredOrders = [
-    0, 902, 903, 215, 216, 210, 355, 356, 357, 360, 609, 619, 614, 615, 621,
-    625, 293, 294,
+    0, 902, 903, 216, 210, 355, 356, 357, 360, 609, 619, 614, 293, 294,
 ];
 
 export function getHardcodedOrders(boardOrders: BoardOrders[]): BoardOrders[] {
