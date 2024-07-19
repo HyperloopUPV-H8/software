fb555794bbb2ee2b5c8526dbe5aa33a87752be2b
jmaralo <martinezalonsojn@gmail.com>
2024-07-19 16:55:58 +0200

    thread safe implementation of ring buffer

--- backend/internal/common/ring_buf.go
+++ backend/internal/common/ring_buf.go
@@ -1,65 +1,75 @@
 package common
 
-type RingBuf[T any] struct {
-	buf  []T
-	curr int
+import "sync"
+
+// RingBuffer is a concurrently safe ring buffer implementation that can be resized anytime
+//
+// Adding values run in constant time but resizing takes linear time
+type RingBuffer[T any] struct {
+	data       chan T
+	resizeLock *sync.Mutex
 }
 
-func NewRingBuf[T any](size int) RingBuf[T] {
-	return RingBuf[T]{
-		buf:  make([]T, size),
-		curr: 0,
+// Create a new ring buffer of the desired size
+func NewRingBuffer[T any](size int) RingBuffer[T] {
+	return RingBuffer[T]{
+		data:       make(chan T, size),
+		resizeLock: new(sync.Mutex),
 	}
 }
 
-func (buf *RingBuf[T]) Add(value T) (evicted T) {
-	evicted = buf.buf[buf.curr]
-	buf.buf[buf.curr] = value
-	buf.curr = buf.next()
-	return evicted
+func (buffer RingBuffer[T]) Len() int {
+	return cap(buffer.data)
 }
 
-func (buf *RingBuf[T]) next() int {
-	return (buf.curr + 1) % buf.Len()
-}
+// Add adds a new value to the buffer
+//
+// If the buffer overflowed the value that had to be removed is returned,
+// otherwise Add returns the default value
+//
+// This function runs in constant time
+func (buffer RingBuffer[T]) Add(value T) (evicted T) {
+	buffer.resizeLock.Lock()
+	defer buffer.resizeLock.Unlock()
 
-func (buf *RingBuf[T]) Len() int {
-	return len(buf.buf)
-}
-
-func (buf *RingBuf[T]) Resize(size uint) []T {
-	if size > (uint)(buf.Len()) {
-		return buf.Grow(size - (uint)(buf.Len()))
-	} else if size < (uint)(buf.Len()) {
-		return buf.Shrink((uint)(buf.Len()) - size)
+	select {
+	case buffer.data <- value:
+	default:
+		evicted = <-buffer.data
+		buffer.data <- value
 	}
-
-	return nil
+	return
 }
 
-func (buf *RingBuf[T]) Shrink(amount uint) []T {
-	removed := make([]T, 0, amount)
+// Resize resizes the ring buffer
+//
+// This function retursn the values that were added / removed from the buffer.
+// The value returned when growing is the default value.
+//
+// This function runs in linear time
+func (buffer RingBuffer[T]) Resize(size int) (changedValues []T) {
+	buffer.resizeLock.Lock()
+	defer buffer.resizeLock.Unlock()
 
-	for i := (uint)(0); i < amount; i++ {
-		removed = append(removed, buf.pop())
-		if buf.curr >= buf.Len() {
-			buf.curr--
+	newData := make(chan T, size)
+resizeLoop:
+	for value := range buffer.data {
+		select {
+		case newData <- value:
+		default:
+			break resizeLoop
 		}
 	}
 
-	return removed
-}
+	if size > cap(buffer.data) {
+		changedValues = make([]T, cap(buffer.data)-len(buffer.data))
+	} else if size < cap(buffer.data) {
+		changedValues = make([]T, 0, len(buffer.data))
+		for remaingValue := range buffer.data {
+			changedValues = append(changedValues, remaingValue)
+		}
+	}
+	buffer.data = newData
 
-func (buf *RingBuf[T]) pop() (removed T) {
-	buf.buf, removed = Remove(buf.buf, buf.curr)
-	return removed
-}
-
-func (buf *RingBuf[T]) Grow(amount uint) []T {
-	added := make([]T, amount)
-
-	head := append(added, buf.buf[buf.curr:]...)
-	buf.buf = append(buf.buf[:buf.curr], head...)
-
-	return added
+	return
 }
