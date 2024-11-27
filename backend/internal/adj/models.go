40a247958749c63d9b79a49088c4dae6bfebefde
msanlli <markix95@gmail.com>
2024-11-27 23:40:31 +0100

    adj models

--- backend/internal/adj/models.go
+++ backend/internal/adj/models.go
@@ -4,3 +4,39 @@ type ADJ struct {
 	Info   Info
 	Boards map[string]Board
 }
+
+type Info struct {
+	Addresses  map[string]string
+	Units      map[string]string
+	Ports      map[string]string
+	BoardIds   map[string]string
+	MessageIds map[string]string
+}
+
+type Board struct {
+	Name         string
+	Packets      []Packet
+	Measurements []Measurement
+	Structures   []Structure
+}
+
+type Packet struct {
+	Id   string
+	Name string
+	Type string
+}
+
+type Measurement struct {
+	Id           string
+	Name         string
+	Type         string
+	PodUnits     string
+	DisplayUnits string
+	SafeRange    string
+	WarningRange string
+}
+
+type Structure struct {
+	Packet       string
+	Measurements []string
+}
