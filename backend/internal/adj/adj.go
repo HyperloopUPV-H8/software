a15ebe595ac6b2e93253c2f9efb6d23f85fe3394
msanlli <markix95@gmail.com>
2024-11-28 02:19:29 +0100

    adj struct unmarshal

--- backend/internal/adj/adj.go
+++ backend/internal/adj/adj.go
@@ -1,29 +1,56 @@
 package adj
 
 import (
+	"encoding/json"
 	"github.com/go-git/go-git/v5"
+	"os"
 )
 
 const (
 	RepoUrl = "https://github.com/HyperloopUPV-H8/JSON_ADE.git" // URL of the ADJ repository
 )
 
-func downloadADJ() error {
+func downloadADJ() (json.RawMessage, json.RawMessage, error) {
 	_, err := git.PlainClone("", false, &git.CloneOptions{
 		URL: RepoUrl,
 	})
 	if err != nil {
-		return err
+		return nil, nil, err
 	}
 
-	return nil
+	info, err := os.ReadFile("general_info.json")
+	if err != nil {
+		return nil, nil, err
+	}
+
+	boards, err := os.ReadFile("boards.json")
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return info, boards, nil
 }
 
 func NewADJ() (*ADJ, error) {
-	err := downloadADJ()
+	infoRaw, boardsRaw, err := downloadADJ()
 	if err != nil {
 		return nil, err
 	}
 
-	return &ADJ{}, nil
+	var info Info
+	if err := json.Unmarshal(infoRaw, &info); err != nil {
+		return nil, err
+	}
+
+	var boards map[string]Board
+	if err := json.Unmarshal(boardsRaw, &boards); err != nil {
+		return nil, err
+	}
+
+	adj := &ADJ{
+		Info:   info,
+		Boards: boards,
+	}
+
+	return adj, nil
 }
