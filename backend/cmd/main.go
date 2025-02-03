ae20ca8894615c424e06eec3d88c54f343062ef9
msanlli <marc@sanchis.dev>
2025-02-03 19:36:15 +0100

    force the pass of ADJ tests

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -12,6 +12,7 @@ import (
 	"net/http"
 	_ "net/http/pprof"
 	"os"
+	"os/exec"
 	"os/signal"
 	"path"
 	"runtime"
@@ -103,6 +104,13 @@ func main() {
 	adj, err := adj_module.NewADJ()
 	if err != nil {
 		trace.Fatal().Err(err).Msg("setting up ADJ")
+	}
+
+	test := exec.Command("python3", "testadj.py")
+	out, err := test.CombinedOutput()
+	if err != nil || len(out) != 0 {
+		fmt.Printf("\nPython test failed:\nError: %v\nOutput: %s\n", err, string(out))
+		os.Exit(1)
 	}
 
 	podData, err := pod_data.NewPodData(adj.Boards, adj.Info.Units)
