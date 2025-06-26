25bc89b3877cd459820df9eab71b6e30802911e3
Joan Física <joanfisica5@gmail.com>
2025-06-26 21:10:09 +0200

    Update log-all and log-none for collapsed boards

--- ethernet-view/src/components/ReceiveTable/ReceiveTable.tsx
+++ ethernet-view/src/components/ReceiveTable/ReceiveTable.tsx
@@ -2,26 +2,29 @@ import styles from "./ReceiveTable.module.scss";
 import { BoardView } from "./BoardView/BoardView";
 import { Header } from "./Header/Header";
 import { TableUpdater } from "./TableUpdater";
-import { Board } from "common";
+import { Board, useMeasurementsStore} from "common";
 
 type Props = {
     boards: Board[];
 };
 
 export const ReceiveTable = ({ boards }: Props) => {
+    const handleLogAll = (log: boolean) => {
+        useMeasurementsStore.getState().setLogAll(log);
+    };
     return (
         <TableUpdater>
             <div className={styles.newReceiveTable}>
                 <div style={{ display: 'flex', alignItems: 'center', marginBottom: 12 }}>
                     <div style={{ flex: 1 }}></div>
                     <button
-                        onClick={() => window.dispatchEvent(new CustomEvent('log-all', { detail: true }))}
+                        onClick={() => handleLogAll(true)}
                         style={{ background: 'green', color: 'white', border: 'none', borderRadius: 4, padding: '2px 8px', cursor: 'pointer', marginRight: 4 }}
                     >
                         Log all
                     </button>
                     <button
-                        onClick={() => window.dispatchEvent(new CustomEvent('log-all', { detail: false }))}
+                        onClick={() => handleLogAll(false)}
                         style={{ background: '#aaa', color: 'white', border: 'none', borderRadius: 4, padding: '2px 8px', cursor: 'pointer' }}
                     >
                         Log none
