baccdb1ae7ab4791f65ab1b2349f2dc8df0e42e0
Joan Física <joanfisica5@gmail.com>
2025-07-05 22:37:41 +0200

    Batteries safe and warning ranges

--- control-station/src/pages/VehiclePage/Data1Page/Data1Modules/Data1Batteries.tsx
+++ control-station/src/pages/VehiclePage/Data1Page/Data1Modules/Data1Batteries.tsx
@@ -23,23 +23,23 @@ export const Batteries = () => {
         <div className={styles.row}>
         <Window2 title="High Voltage" className={styles.voltage}>
             <Gauge id="hv" sweep={180} strokeWidth={15} percentage={75} className="" />
-            <BatteryIndicator name={'High Voltage'} getValue={TotalVoltageHigh.getUpdate} 
-            safeRangeMin={TotalVoltageHigh.safeRangeMin} warningRangeMin={TotalVoltageHigh.warningRangeMin} 
-            safeRangeMax={TotalVoltageHigh.safeRangeMax} warningRangeMax={TotalVoltageHigh.warningRangeMax} />
+            <BatteryIndicator name={'High Voltage'} getValue={TotalVoltageHigh.getUpdate}
+                safeRangeMin={TotalVoltageHigh.range[0] ?? 0} warningRangeMin={TotalVoltageHigh.warningRange[0] ?? 0}
+                safeRangeMax={TotalVoltageHigh.range[1] ?? 0} warningRangeMax={TotalVoltageHigh.warningRange[1] ?? 0} />
         </Window2>
 
         <Window2 title="Low Voltage" className={styles.voltage}>
             <Gauge id="lv" sweep={180} strokeWidth={15} percentage={45} className="" />
-            <BatteryIndicator name={'Low Voltage'} getValue={TotalVoltageLow.getUpdate} 
-            safeRangeMin={TotalVoltageLow.safeRangeMin} warningRangeMin={TotalVoltageLow.warningRangeMin} 
-            safeRangeMax={TotalVoltageLow.safeRangeMax} warningRangeMax={TotalVoltageLow.warningRangeMax} />
+            <BatteryIndicator name={'Low Voltage'} getValue={TotalVoltageLow.getUpdate}
+                safeRangeMin={TotalVoltageLow.range[0] ?? 0} warningRangeMin={TotalVoltageLow.warningRange[0] ?? 0}
+                safeRangeMax={TotalVoltageLow.range[1] ?? 0} warningRangeMax={TotalVoltageLow.warningRange[1] ?? 0} />
         </Window2>
 
         <Window2 title="BOOSTER" className={styles.voltage}>
             <Gauge id="boost" sweep={180} strokeWidth={15} percentage={55} className="" />
-            <BatteryIndicator name={'Cabinet Voltage'} getValue={TotalVoltageCabinet.getUpdate} 
-            safeRangeMin={TotalVoltageCabinet.safeRangeMin} warningRangeMin={TotalVoltageCabinet.warningRangeMin} 
-            safeRangeMax={TotalVoltageCabinet.safeRangeMax} warningRangeMax={TotalVoltageCabinet.warningRangeMax} />
+            <BatteryIndicator name={'Cabinet Voltage'} getValue={TotalVoltageCabinet.getUpdate}
+                safeRangeMin={TotalVoltageCabinet.range[0] ?? 0} warningRangeMin={TotalVoltageCabinet.warningRange[0] ?? 0}
+                safeRangeMax={TotalVoltageCabinet.range[1] ?? 0} warningRangeMax={TotalVoltageCabinet.warningRange[1] ?? 0} />
         </Window2>
     </div>
     );
