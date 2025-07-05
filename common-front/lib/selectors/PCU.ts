37d5d2f4c516b9457f3dcbd8cad24b60a67b7f17
Joan Física <joanfisica5@gmail.com>
2025-07-05 19:10:43 +0200

    Update PcuMeasurements

--- common-front/lib/selectors/PCU.ts
+++ common-front/lib/selectors/PCU.ts
@@ -1,5 +1,3 @@
-import { Measurements, NumericMeasurement, useMeasurementsStore } from '..';
-
 export const PcuMeasurements = {
     motorAPeakCurrent: 'PCU/peak_current',
     motorACurrentU: 'PCU/current_sensor_u_a',
@@ -7,13 +5,11 @@ export const PcuMeasurements = {
     motorACurrentW: 'PCU/current_sensor_w_a',
     motorATemp: 'PCU/motor_a_temp_u',
     motorBPeakCurrent: 'PCU/peak_current',
-    motorBCurrentU: 'PCU/current_sensor_u_a',
-    motorBCurrentV: 'PCU/current_sensor_v_a',
-    motorBCurrentW: 'PCU/current_sensor_w_a',
+    motorBCurrentU: 'PCU/current_sensor_u_b',
+    motorBCurrentV: 'PCU/current_sensor_v_b',
+    motorBCurrentW: 'PCU/current_sensor_w_b',
     motorBTemp: 'PCU/motor_b_temp_v',
     frequency: 'PCU/target_frequency',
     generalState: 'PCU/general_state',
     specificState: 'PCU/operating_state',
 };
-
-export const PcuOrders = {};
