d1674f17f1c30acf9f38e26e24a38aab85b5e433
Joan Física <joanfisica5@gmail.com>
2025-07-06 15:27:01 +0200

    Fix button routes

--- control-station/src/pages/VehiclePage/vehicleRoute.tsx
+++ control-station/src/pages/VehiclePage/vehicleRoute.tsx
@@ -7,7 +7,7 @@ export const vehicleRoute = {
     path: '/vehicle',
     element: <VehiclePage />,
     children: [
-        { path: '', element: <Navigate to={'guiBooster'} /> },
+        { path: '', element: <Navigate to={'data-1'} /> },
         { path: 'data-1', element: <Data1Page /> },
         { path: 'data-2', element: <Data2Page /> },
         { path: 'guiBooster', element: <GuiPage /> },
