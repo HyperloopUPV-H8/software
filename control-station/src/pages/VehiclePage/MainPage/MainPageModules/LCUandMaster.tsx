3f33f8df3a6ad8f70529d69fbd61c68aed695a3e
Joan Física <joanfisica5@gmail.com>
2025-07-09 19:36:54 +0200

    Use team logo

--- control-station/src/pages/VehiclePage/MainPage/MainPageModules/LCUandMaster.tsx
+++ control-station/src/pages/VehiclePage/MainPage/MainPageModules/LCUandMaster.tsx
@@ -1,6 +1,7 @@
 import { VcuMeasurements } from "common";
 import { StateIndicator } from "components/StateIndicator/StateIndicator";
 import { Window } from "components/Window/Window";
+import teamLogo from "assets/svg/team_logo.svg"
 import { LCU } from "pages/VehiclePage/Boards/LCU/LCU";
 import styles from "../MainPage.module.scss"
 
@@ -10,7 +11,7 @@ export const LCUandMaster = () => {
         <div className={styles.LCUandMaster}>
         <LCU/>
         <Window title="Vehicle State">
-            <StateIndicator measurementId={VcuMeasurements.operationalState} icon={""} />
+            <StateIndicator measurementId={VcuMeasurements.operationalState} icon={teamLogo} />
         </Window>
         </div>
     );
