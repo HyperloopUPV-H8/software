37f24a55b8340cf12159668128a47d170b339f6f
Joan Física <joanfisica5@gmail.com>
2025-07-10 13:16:54 +0200

    Change desired orders for main page

--- control-station/src/pages/VehiclePage/BatteriesPage/FixedOrders.tsx
+++ control-station/src/pages/VehiclePage/BatteriesPage/FixedOrders.tsx
@@ -54,8 +54,7 @@ export const emergencyStopOrders: Order[] = [
 ];
 
 export const desiredOrders = [
-    0, 902, 903, 216, 210, 355, 356, 357, 360, 609, 619, 614, 615, 363, 364,
-    293, 294, 645, 646, 1699, 1698
+    53, 44, 52, 43, 37, 46, 38, 47, 62, 55
 ];
 
 export function getHardcodedOrders(boardOrders: BoardOrders[]): BoardOrders[] {
