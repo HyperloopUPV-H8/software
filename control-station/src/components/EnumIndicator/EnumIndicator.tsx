98748760e21b5c2f843a5c77cc1abd9ad463d759
Marc Sanchis <marc@sanchis.dev>
2025-07-12 17:39:17 +0200

    add FAULT to EnumIndicator

--- control-station/src/components/EnumIndicator/EnumIndicator.tsx
+++ control-station/src/components/EnumIndicator/EnumIndicator.tsx
@@ -1,59 +1,58 @@
-import { useGlobalTicker, useMeasurementsStore } from 'common';
-import styles from './EnumIndicator.module.scss';
-import { memo, useContext, useState } from 'react';
-import { LostConnectionContext } from 'services/connections';
+import { useGlobalTicker, useMeasurementsStore } from "common";
+import styles from "./EnumIndicator.module.scss";
+import { memo, useContext, useState } from "react";
+import { LostConnectionContext } from "services/connections";
 
 interface Props {
-    measurementId: string;
-    icon: string;
+  measurementId: string;
+  icon: string;
 }
 
 export const EnumIndicator = memo(({ measurementId, icon }: Props) => {
-    const getValue = useMeasurementsStore(
-        (state) => state.getEnumMeasurementInfo(measurementId).getUpdate
-    );
+  const getValue = useMeasurementsStore(
+    (state) => state.getEnumMeasurementInfo(measurementId).getUpdate,
+  );
 
-    const lostConnection = useContext(LostConnectionContext);
+  const lostConnection = useContext(LostConnectionContext);
 
-    const [variant, setVariant] = useState(getValue());
-    const state = lostConnection
-        ? 'DISCONNECTED'
-        : variant;
+  const [variant, setVariant] = useState(getValue());
+  const state = lostConnection ? "DISCONNECTED" : variant;
 
-    useGlobalTicker(() => {
-        setVariant(getValue());
-    });
+  useGlobalTicker(() => {
+    setVariant(getValue());
+  });
 
-    return (
-        <div
-            className={styles.enum_indicator}
-            style={{ backgroundColor: enumToColor[state.toUpperCase()] || enumToColor.default }}
-        >
-            <img className={styles.icon} src={icon} />
+  return (
+    <div
+      className={styles.enum_indicator}
+      style={{
+        backgroundColor:
+          enumToColor[state.toUpperCase()] || enumToColor.default,
+      }}
+    >
+      <img className={styles.icon} src={icon} />
 
-            <p className={styles.title}>
-                {lostConnection ? 'DISCONNECTED' : state}
-            </p>
+      <p className={styles.title}>{lostConnection ? "DISCONNECTED" : state}</p>
 
-            <img className={styles.icon} src={icon} />
-        </div>
-    );
+      <img className={styles.icon} src={icon} />
+    </div>
+  );
 });
 
 const enumToColor: { [key: string]: string } = {
-    'DISCONNECTED' : '#cccccc',
+  DISCONNECTED: "#cccccc",
 
-    'NOT_CHARGING' : '#EBF6FF',
-    'CHARGING' : '#F28E31',
+  NOT_CHARGING: "#EBF6FF",
+  CHARGING: "#F28E31",
 
-    'OPEN' : '#ACF293',
-    'PRECHARGE' : '#F4F788',
-    'CLOSED' : '#F28E31',
-    'CLOSE' : '#F28E31',
+  OPEN: "#ACF293",
+  PRECHARGE: "#F4F788",
+  CLOSED: "#F28E31",
+  CLOSE: "#F28E31",
 
-    'DISENGAGED' : '#EF9A87',
-    'ENGAGED' : '#ACF293',
+  DISENGAGED: "#EF9A87",
+  ENGAGED: "#ACF293",
+  FAULT: "#EF9A87",
 
-    default : '#EBF6FF'
-
-}
+  default: "#EBF6FF",
+};
