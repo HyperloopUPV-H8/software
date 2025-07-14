8bcff1b70d46976b1fe5e14d76eb9138f920b3b3
Marc Sanchis <marc@sanchis.dev>
2025-07-14 12:47:02 +0200

    Add orange background and border to Orders module

--- ethernet-view/src/pages/TestingPage/OrderColumn/OrderColumn.tsx
+++ ethernet-view/src/pages/TestingPage/OrderColumn/OrderColumn.tsx
@@ -3,6 +3,7 @@ import { nanoid } from "nanoid";
 import { OrdersContainer } from "components/OrdersContainer/OrdersContainer";
 import { useRef } from "react";
 import { ReactComponent as OutgoingMessage } from "assets/svg/outgoing-message.svg";
+import styles from "./OrderColumn.module.scss";
 
 export const OrderColumn = () => {
     const orderTabItems = useRef([
@@ -14,5 +15,7 @@ export const OrderColumn = () => {
         },
     ]);
 
-    return <TabLayout tabs={orderTabItems.current}></TabLayout>;
+    return <div className={styles.orderColumn}>
+        <TabLayout tabs={orderTabItems.current}></TabLayout>
+    </div>;
 };
